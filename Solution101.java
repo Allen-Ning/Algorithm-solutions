2937702b986cc737bca343a2386820224376e9ee
Allen Ning <npc1222@icloud.com>
2018-09-13 20:49:06 +1000

    added solution 101

---
+++ Solution101.java
@@ -0,0 +1,25 @@
+  /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution101 {
+
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    return helper(root.left, root.right);
+  }
+
+  private boolean helper(TreeNode leftNode, TreeNode rightNode) {
+    if (leftNode == null || rightNode == null) {
+      return leftNode == rightNode;
+    } else if (leftNode.val != rightNode.val) {
+      return false;
+    }
+    return helper(leftNode.left, rightNode.right) && helper(leftNode.right, rightNode.left);
+  }
+}

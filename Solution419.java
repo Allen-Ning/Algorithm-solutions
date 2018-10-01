92f0ebc13229c21e3003ceead79f741d23c749ac
Allen Ning <npc1222@icloud.com>
2018-10-02 00:39:41 +1000

    added Solution 419

---
+++ Solution419.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        if (board == null || board.length == 0) return 0;
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+          for (int j = 0; j < board[0].length; j++) {
+            if (board[i][j] == 'X' && 
+                (j == 0 || board[i][j - 1] == '.') && 
+                (i == 0 || board[i-1][j] == '.')
+                ) {
+                count++;
+            } 
+          }
+        }
+        return count;
+    }
+}

245e048400c6dae0e9059fd0c35e596291323a60
Allen Ning <npc1222@icloud.com>
2019-01-08 08:23:31 +1100

    added solution 48

---
+++ Solution48.java
@@ -0,0 +1,22 @@
+class Solution {
+  public void rotate(int[][] matrix) {
+    int rows = matrix.length;
+    int columns = matrix[0].length;
+
+    for (int i = 0; i < rows/2; i++ ) {
+      for (int j = 0; j < columns; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[rows - i - 1][j];
+        matrix[rows - i - 1][j] = temp;
+      }
+    }
+
+    for(int i = 0; i < rows; i ++) {
+      for (int j = i; j < columns; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+  }
+}

d0ba09ac5695221ae4fe7d56f7c75901573c9213
Allen Ning <npc1222@icloud.com>
2019-02-09 18:17:28 +1100

    added solutiom 80

---
+++ Solution80.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int removeDuplicates(int[] nums) {
+    if (nums.length <= 2) return nums.length;
+    
+    int result = 2;
+    for (int i = 2; i < nums.length; i++) {
+      if (nums[i] == nums[result - 2]) {
+        continue;
+      } else {
+        nums[result] = nums[i];
+        result++;
+      }
+    }
+    return result;
+  }
+}

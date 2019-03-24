17ea182b7ec62b6a1caebc100a9e44f75e5279b2
Allen Ning <npc1222@icloud.com>
2019-03-24 14:32:20 +1100

    added Solution 206

---
+++ Solution206.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode reverseList(ListNode head) {
+    ListNode current = head;
+    ListNode pre = null;
+    ListNode next = current.next;
+    while(current != null) {
+      current.next = pre;
+      pre = current;
+      current = next;
+      if (next != null) next = next.next;
+    }
+    return pre;
+  }
+}

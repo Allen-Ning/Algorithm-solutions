4b616ad74d6288d2da7c1fd5fda290c7bdcfed1d
Allen Ning <npc1222@icloud.com>
2019-05-04 19:17:25 +1000

    much clean solution

--- Solution228.java
+++ Solution228.java
@@ -1,33 +1,18 @@
 class Solution {
   public List<String> summaryRanges(int[] nums) {
-    int start = 0;
-    int end = 0;
-    ArrayList<String> values = new ArrayList();
+    ArrayList<String> list = new ArrayList();
     for (int i = 0; i < nums.length; i++) {
-      if ( i == nums.length - 1) {
-        if (i >= 1 && nums[i] == nums[i - 1] + 1) {
-          end = i;
-          add(nums, values, start, end);
-        } else {
-          end = i;
-          add(nums, values, end, end);
-        }
+      int value = nums[i];
+      while (i != nums.length - 1 && nums[i] + 1 == nums[i + 1]) {
+        i++;
+      }
+
+      if (nums[i] > value) {
+        list.add(value + "->" + nums[i]);
       } else {
-        if (nums[i] + 1 != nums[i + 1]) {
-          end = i;
-          add(nums, values, start, end);
-          start = i + 1;
-        }
+        list.add(nums[i] + "");
       }
     }
-    return values;
-  }
-
-  private void add(int[] nums, ArrayList values, int start, int end) {
-    if (end > start) {
-      values.add(nums[start] + "->" +  nums[end]);
-    } else {
-      values.add(Integer.toString(nums[start]));
-    }
+    return list;
   }
 }

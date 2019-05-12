c6bf8ab71d35911ff0f02bfaf84949da18cf4f32
Allen Ning <npc1222@icloud.com>
2019-05-12 20:49:41 +1000

    added a more cleanup version

--- Solution238.java
+++ Solution238.java
@@ -1,18 +1,17 @@
 class Solution {
   public int[] productExceptSelf(int[] nums) {
-    int[] leftSum = new int[nums.length];
-    leftSum[0] = 1;
+    int[] sum = new int[nums.length];
+    sum[0] = 1;
 
     for (int i = 1; i < nums.length; i++) {
-      leftSum[i] = leftSum[i - 1] * nums[i - 1];
+      sum[i] = sum[i - 1] * nums[i - 1];
     }
     
     int right = 1;
-    int[] total = new int[nums.length];
     for (int i = nums.length - 1; i >= 0; i--) {
-      total[i] = leftSum[i] * right;
+      sum[i] = sum[i] * right;
       right = right * nums[i];
     }
-    return total;
+    return sum;
   }
 }

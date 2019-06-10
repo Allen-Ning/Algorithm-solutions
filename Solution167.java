d81fd8683cdc2ef3108462d5b4b3f976bb8f20c4
Allen Ning <npc1222@icloud.com>
2019-06-10 12:24:24 +1000

    make the code better

--- Solution167.java
+++ Solution167.java
@@ -1,21 +1,15 @@
 class Solution {
   public int[] twoSum(int[] numbers, int target) {
-    for (int i = 0; i < numbers.length; i++) {
-      int lookupNumber = target - numbers[i];
-
-      // binary search
-      int left = 0;
-      int right = numbers.length;
-      while (left < right) {
-        int mid = left + (right - left) / 2;
-          if (numbers[mid] == lookupNumber) {
-          return (i < mid) ? new int[] {i + 1, mid + 1} : new int[] {mid + 1, i + 1};
-        }
-        if (numbers[mid] > lookupNumber) {
-          right = mid;
-        } else {
-          left = mid + 1;
-        }
+    int start = 0;
+    int end = numbers.length - 1;
+    while (start < end) {
+      int sum = numbers[start] + numbers[end];
+      if (sum == target) {
+        return new int [] { start + 1, end + 1 };
+      } else if (sum > target) {
+        end--;
+      } else if (sum < target) {
+        start++;
       }
     }
     return null;

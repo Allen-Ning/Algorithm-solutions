a72f756f7eb959e00ea1af9c676c7debf2f13826
Allen Ning <npc1222@icloud.com>
2019-06-11 17:07:57 +1000

    minor change for solution 118

--- Solution118.java
+++ Solution118.java
@@ -1,22 +1,20 @@
 class Solution {
-  public List<List<Integer>> generate(int numRows) {
-    List<List<Integer>> result = new ArrayList();
-    if (numRows == 0) return result;
-    List<Integer> list = new ArrayList();
-    list.add(1);
-    result.add(list);
-
-    for (int i = 1; i < numRows; i++) {
-      List<Integer> list2 = new ArrayList();
-      for (int j = 0; j <= i; j++) {
-        List<Integer> aboveRow = result.get(i - 1);
-        int value = 0;
-        if (j - 1 >= 0) value += aboveRow.get(j - 1);
-        if (j <= aboveRow.size() - 1) value += aboveRow.get(j);
-        list2.add(value);
-      }
-      result.add(list2);
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList();
+        if (numRows == 0) return result;
+        
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> list = new ArrayList();
+            list.add(1);
+            for (int j = 1; j <= i; j++) {
+                List<Integer> aboveRow = result.get(i - 1);
+                int value = 0;
+                if (j - 1 >= 0) value += aboveRow.get(j - 1);
+                if (j <= aboveRow.size() - 1) value += aboveRow.get(j);
+                list.add(value);
+            }
+            result.add(list);
+        }
+        return result;
     }
-    return result;
-  }
 }

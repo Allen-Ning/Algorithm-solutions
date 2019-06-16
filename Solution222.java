88bef06c31ff32223c079d6b6893c862818a9362
Allen Ning <npc1222@icloud.com>
2019-06-16 22:26:28 +1000

    better solution

--- Solution222.java
+++ Solution222.java
@@ -8,34 +8,30 @@
  * }
  */
 class Solution {
-
+    
     public int countNodes(TreeNode root) {
         return helper(root);
     }
-
-    private int checkLeft(TreeNode node) {
-        if (node == null) return 0;
-        int height = checkLeft(node.left);
-        return height + 1;
-    }
-
-    private int checkRight(TreeNode node) {
-        if (node == null) return 0;
-        int height = checkRight(node.right);
-        return height + 1;
-    }
-
+    
     private int helper(TreeNode node) {
         if (node == null) {
             return 0;
         }
-
-        int leftHeight = checkLeft(node);
-        int rightHeight = checkRight(node);
-        if (leftHeight == rightHeight) {
-            return (int)Math.pow(2, leftHeight) - 1;
+        
+        TreeNode left = node;
+        TreeNode right = node;
+        int height = 0;
+        
+        while (right != null) {
+            left = left.left;
+            right = right.right;
+            height++;
+        }
+        
+        if (left == null) {
+            return (1 << height) - 1;
         } else {
             return helper(node.left) + helper(node.right) + 1;
-        }
-    }
+        } 
+    }   
 }

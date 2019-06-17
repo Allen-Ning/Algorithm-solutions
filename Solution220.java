c1e991c9a24a52cd5564921f2fc6a24af30f0c89
Allen Ning <npc1222@icloud.com>
2019-06-17 23:53:01 +1000

    provide o(n) time complexity for solution 220

--- Solution220.java
+++ Solution220.java
@@ -1,25 +1,24 @@
-public class Solution {
-    // *  |-----*------|  *
+class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null || nums.length == 0 || k <= 0) {
-            return false;
-        }
+        if (nums == null || nums.length == 0) return false;
 
-        TreeSet<Integer> values = new TreeSet<>();
-        for (int index = 0; index < nums.length; index++) {
-            Integer floor = values.floor(nums[index]);
-            Integer ceil = values.ceiling(nums[index]);
-            long value = Long.valueOf(nums[index]);
-            if ((floor != null && Math.abs(value - Long.valueOf(floor)) <= t) || (ceil != null && Math.abs(value - Long.valueOf(ceil)) <= t)) {
-                return true;
+        // find min value
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) min = Math.min(min, num);
+
+        HashMap<Long, Long> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            long value = Long.valueOf(nums[i]);
+            long key = (value -  Long.valueOf(min)) / (Math.abs(Long.valueOf(t)) + 1);
+            if (map.containsKey(key) && Math.abs(map.get(key) - value) <= t) return true;
+            if (map.containsKey(key - 1) && Math.abs(map.get(key - 1) - value) <= t) return true;
+            if (map.containsKey(key + 1) && Math.abs(map.get(key + 1) - value) <= t) return true;
+            map.put(key, value);
+
+            if (i >= k) {
+                map.remove((Long.valueOf(nums[i - k]) - min) / (Math.abs(Long.valueOf(t)) + 1));
             }
-
-            values.add(nums[index]);
-            if (index >= k) {
-                values.remove(nums[index - k]);
-            }
         }
-
         return false;
     }
 }

bd5c8f8bd2d78793c206a14728aa5585c48f5597
Allen Ning <npc1222@icloud.com>
2019-06-17 21:19:14 +1000

    added solution 242

---
+++ Solution242.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] letters = new int[26];
+        
+        for(int i = 0; i < s.length(); i++) letters[s.charAt(i) - 'a'] += 1;
+        for(int i = 0; i < t.length(); i++) letters[t.charAt(i) - 'a'] -= 1;
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] != 0) return false;
+        }
+
+        return true;
+    }
+}

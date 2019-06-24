5395c941f51b240445c4834da802ab541f172ea7
Allen Ning <npc1222@icloud.com>
2019-06-24 20:05:56 +1000

    provide a better solution

--- Solution415.java
+++ Solution415.java
@@ -3,23 +3,13 @@ class Solution {
         if (num1.length() == 0) return num2;
         if (num2.length() == 0) return num1;
         int carry = 0;
-        while (num1.length() < num2.length()) {
-            num1 =  "0" + num1; 
-        }
-        while (num1.length() > num2.length()) {
-            num2 = "0" + num2; 
-        }
-      
         StringBuilder sb = new StringBuilder();
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            char c1 = num1.charAt(i);
-            char c2 = num2.charAt(i);
-            int value = ((c1 - '0') + (c2 - '0') + carry) % 10;
-            carry = ((c1 - '0') + (c2 - '0') + carry) / 10;
-            
-            sb.append(value);
+        for (int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >=0 || carry == 1; i--, j--) {
+            int value1 = i < 0 ? 0 : (num1.charAt(i) - '0');
+            int value2 = j < 0 ? 0 : (num2.charAt(j) - '0');
+            sb.append((value1 + value2 + carry) % 10);
+            carry = (value1 + value2 + carry) / 10; 
         }
-        if (carry == 1) sb.append(carry);
         return sb.reverse().toString();
     }
 }

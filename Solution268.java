7f64f0b5789afb942fb8c3a702e12cf321f86b4b
Allen Ning <npc1222@icloud.com>
2019-07-24 23:26:07 +1000

    added another answer for solution 268

--- Solution268.java
+++ Solution268.java
@@ -7,4 +7,16 @@ class Solution {
         }
         return total - subTotal;
     }
+
+    // a ^ a = 0
+    // b ^ 0 = b
+    // b ^ a ^ a = b
+    public int missingNumber2(int[] nums) {
+        int result = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= i;
+            result ^= nums[i];
+        }
+        return result;
+    }
 }

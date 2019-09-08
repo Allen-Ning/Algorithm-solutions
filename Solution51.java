dfc0d19cb91b21042d6bea06dd64f51a16c02066
Allen Ning <npc1222@icloud.com>
2019-09-08 15:00:57 +1000

    added a polished answer for solution 51

--- Solution51.java
+++ Solution51.java
@@ -1,47 +1,58 @@
 class Solution {
-  List<List<String>> results = new ArrayList();
   public List<List<String>> solveNQueens(int n) {
-    helper(new int[n], 0);
-    return null;
+    List<List<String>> results = new ArrayList();
+    helper(new int[n], 0, results);
+    return results;
   }
 
-  private void helper(int[] queens, int current) {
-    // enough number
-    if (current >= n) {
+  private void helper(int[] queens, int current, List<List<String>> results) {
+    if (current >= queens.length) {
       List<String> result = generateResult(queens);
       results.add(result);
       return;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < queens.length; i++) {
       if (!canPlace(queens, current, i)) continue;
-      queendds[current] = i;
-      helper(queens, current + 1);
+      queens[current] = i;
+      helper(queens, current + 1, results);
     }
   }
 
   private boolean canPlace(int[] queens, int current, int position) {
-    boolean checkTopLeft = false;
-    int total = current;
-    int currentX = current;
-    // check top
+    int total = current + 1;
+    int currentX = current - 1;
     int currentY = position;
+
     // check leftTop
     int currentLeftY = position;
     // check rirghtTop
     int currentRightY = position;
 
     while (total > 0) {
-      currentX--;
-      total--;
+      if (currentX < 0) return true;
       if (queens[currentX] == --currentLeftY ||
           queens[currentX] == currentY ||
-          queens[currentX] == ++currentY) return false;
+          queens[currentX] == ++currentRightY) return false;
+        total--;
+        currentX--;
     }
     return true;
   }
 
   private List<String> generateResult(int[] queens) {
-    return null; 
+    List<String> list = new ArrayList();
+    for (int i = 0; i < queens.length; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < queens.length; j++) {
+            if (queens[i] == j) {
+                sb.append('Q');
+            } else {
+                sb.append('.');
+            }
+        }
+        list.add(sb.toString());
+    }
+    return list;
   }
 }

6482adbf5e27303fb41a4c9513edec4036bb1133
Allen Ning <npc1222@icloud.com>
2019-10-21 22:40:06 +1100

    added solution 515

---
+++ Solution515.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> results = new ArrayList();
+        LinkedList<TreeNode> queue = new LinkedList();
+        if (root == null) return results;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                max = Math.max(max, node.val);
+            }
+            results.add(max);
+        }
+        return results;
+    }
+}

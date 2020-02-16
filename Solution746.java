f8c3ed7e76d7778c1a364909701203cc5eb930b2
Allen Ning <npc1222@icloud.com>
2020-02-16 20:34:28 +1100

    added solution 746

---
+++ Solution746.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost == null || cost.length == 0) return 0;
+        if (cost.length == 1) return cost[0];
+
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+        return Math.min(dp[cost.length -1], dp[cost.length - 2]);
+    }
+}

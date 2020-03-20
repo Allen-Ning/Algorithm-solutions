1880e1f915336096b868bf708249aac0b2715569
Allen Ning <npc1222@icloud.com>
2020-03-20 23:28:56 +1100

    added solution 714

---
+++ solution714.java
@@ -0,0 +1,20 @@
+class Solution {
+    //        i
+    // [1, 3, 2, 8, 4, 9]
+    public int maxProfit(int[] prices, int fee) {
+        // trick -> one dimension dp quesiton with `two state`
+        //          linear scan
+        int[] buyable = new int[prices.length];
+        int[] sellable = new int[prices.length];
+
+        buyable[0] = 0;
+        sellable[0] = -prices[0];
+        int result = 0;
+        for (int i = 1; i < prices.length; i++) {
+            buyable[i] = Math.max(sellable[i - 1] + prices[i] - fee, buyable[i- 1]);
+            sellable[i] = Math.max(buyable[i- 1] - prices[i], sellable[i - 1]);
+            result = Math.max(result, Math.max(buyable[i], sellable[i]));
+        }
+        return result;
+    }
+}

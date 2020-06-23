c6717b6e71bae0a8585759bcd4894ef10733a913
Allen Ning <npc1222@icloud.com>
2020-06-24 00:49:04 +1000

    added raw version of solution 123

--- Solution123.java
+++ Solution123.java
@@ -21,4 +21,42 @@ class Solution {
         }
         return (int) result;
     }
+
+    public int maxProfitRawVersion(int[] prices) {
+        int days = prices.length + 1;
+        long[][][] dp = new long[3][days][2];
+        dp[0][0][1] = Integer.MIN_VALUE;
+        dp[0][0][0] = 0;
+        dp[1][0][0] = Integer.MIN_VALUE;
+        dp[1][0][1] = Integer.MIN_VALUE;
+        dp[2][0][0] = Integer.MIN_VALUE;
+        dp[2][0][1] = Integer.MIN_VALUE;
+        for (int i = 1; i < days; i++) {
+            int price = prices[i - 1];
+            // 0 transaction, day: i, current status: buyable
+            dp[0][i][0] = dp[0][i - 1][0];
+            // 0 transaction, day: i, current status: sellable
+            dp[0][i][1] = Math.max(dp[0][i - 1][0] - price, dp[0][i - 1][1]);
+
+            // 1 transaction, day: i, current status: buyable
+            dp[1][i][0] = Math.max(dp[0][i - 1][1] + price, dp[1][i - 1][0]);
+            // 1 transaction, day: i, current status: sellable
+            dp[1][i][1] = Math.max(dp[1][i - 1][0] - price, dp[1][i - 1][1]);
+
+            // 2 transaction, day: i, current status: buyable
+            dp[2][i][0] = Math.max(dp[1][i - 1][1] + price, dp[2][i - 1][0]);
+            // 2 transaction, day: i, current status: buyable
+            dp[2][i][1] = Math.max(dp[2][i - 1][0] - price, dp[2][i - 1][1]);
+        }
+
+        long result = 0;
+        result = Math.max(result, dp[0][days - 1][0]);
+        result = Math.max(result, dp[0][days - 1][1]);
+        result = Math.max(result, dp[1][days - 1][0]);
+        result = Math.max(result, dp[1][days - 1][1]);
+        result = Math.max(result, dp[2][days - 1][0]);
+        result = Math.max(result, dp[2][days - 1][1]);
+        return (int) result;
+    }
+
 }

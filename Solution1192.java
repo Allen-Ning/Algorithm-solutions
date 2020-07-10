d30cb7d6cca980585f588f72f3046f49a09d1277
Allen Ning <npc1222@icloud.com>
2020-07-11 02:24:02 +1000

    use array instead of hashmap to make the code run faster

--- Solution1192.java
+++ Solution1192.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        Map<Integer, List<Integer>> map = buildMap(connections);
+        List<Integer>[] map = buildMap(n, connections);
         boolean[] isVisited = new boolean[n];
         int[] steps = new int[n];
         List<List<Integer>> results = new ArrayList();
@@ -8,27 +8,24 @@ class Solution {
         return results;
     }
 
-    private Map<Integer, List<Integer>> buildMap(List<List<Integer>> connections) {
-        HashMap<Integer, List<Integer>> map = new HashMap();
+    private List<Integer>[] buildMap(int n, List<List<Integer>> connections) {
+        List<Integer>[] map = new ArrayList[n];
+        for (int i = 0; i < n; i++) map[i] = new ArrayList<Integer>();
+
         for (List<Integer> connection : connections) {
             int value1 = connection.get(0);
             int value2 = connection.get(1);
-            List<Integer> list = map.getOrDefault(value1, new ArrayList<Integer>());
-            list.add(value2);
-            map.put(value1, list);
-
-            list = map.getOrDefault(value2, new ArrayList<Integer>());
-            list.add(value1);
-            map.put(value2, list);
+            map[value1].add(value2);
+            map[value2].add(value1);
         }
         return map;
     }
 
-    private int helper(Map<Integer, List<Integer>> map, List<List<Integer>> results, boolean[] isVisited, int[] steps, int prev, int current, int step) {
+    private int helper(List<Integer>[] map, List<List<Integer>> results, boolean[] isVisited, int[] steps, int prev, int current, int step) {
         isVisited[current] = true;
         int returnedStep = step;
         steps[current] = step;
-        for (int next : map.get(current)) {
+        for (int next : map[current]) {
             if (next == prev) continue;
             if (isVisited[next]) {
                 returnedStep = Math.min(returnedStep, steps[next]);

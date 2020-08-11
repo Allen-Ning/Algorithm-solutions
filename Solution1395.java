36a1f611dc9fd40f30e694ef16d608c3d97a411f
Allen Ning <npc1222@icloud.com>
2020-08-11 22:48:33 +1000

    added a better solution 1395

--- Solution1395.java
+++ Solution1395.java
@@ -24,4 +24,49 @@ class Solution {
         }
         return result;
     }
+
+    public int numTeams2(int[] rating) {
+        int result = 0;
+
+        FenwichTree left = new FenwichTree((int)1e5 + 1);
+        FenwichTree right = new FenwichTree((int)1e5 + 1);
+
+        for (int score : rating) right.update(score, 1);
+
+        for (int score : rating) {
+            int leftSmallerNum = left.query(score - 1);
+            int leftLargerNum = left.query((int)1e5) - left.query(score);
+            right.update(score, -1);
+            left.update(score, 1);
+
+            int rightLargerNum = right.query((int)1e5) - right.query(score);
+            int rightSmallerNum = right.query(score - 1);
+            result += leftSmallerNum * rightLargerNum + leftLargerNum * rightSmallerNum;
+        }
+        return result;
+    }
+
+    class FenwichTree {
+        int[] data;
+
+        public FenwichTree(int n) {
+            this.data = new int[n];
+        }
+
+        public void update(int index, int delta) {
+            while (index < data.length) {
+                data[index] += delta;
+                index += index & (-index);
+            }
+        }
+
+        public int query(int index) {
+            int sum = 0;
+            while (index > 0) {
+                sum += data[index];
+                index -= index & (-index);
+            }
+            return sum;
+        }
+    }
 }

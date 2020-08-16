84604cdcef5898d01713c2d621e899fc05b7dfe7
Allen Ning <npc1222@icloud.com>
2020-08-16 21:14:25 +1000

    updated notes for solution 243

--- locked-questions/Solution243.java
+++ locked-questions/Solution243.java
@@ -1,11 +1,7 @@
 public class Solution {
-    /**
-     * @param words: a list of words
-     * @param word1: a string
-     * @param word2: a string
-     * @return: the shortest distance between word1 and word2 in the list
-     */
-    public int shortestDistance(String[] words, String word1, String word2) {
+  // trick -> for each word1, find the recent word2
+  //          for each word2, find the recent word1
+  public int shortestDistance(String[] words, String word1, String word2) {
         int index1 = -1;
         int index2 = -1;
         int result = Integer.MAX_VALUE;

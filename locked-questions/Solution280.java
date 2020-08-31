110c5e34ce3500dc9e5fd81af95c74947ab689da
Allen Ning <npc1222@icloud.com>
2020-08-31 20:37:02 +1000

    added solution 280

---
+++ locked-questions/Solution280.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /*
+     * @param nums: A list of integers
+     * @return: nothing
+     */
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        // trick -> e.g.
+        //  0  1  2  3  4  5  6  7
+        // [1, 2, 3, 4, 5, 6, 7, 8]
+        //  1  3  2  5  4  7  6  8
+        for (int i = 1; i < nums.length; i += 2) {
+            if (i + 1 > nums.length - 1) break;
+            swop(nums, i, i + 1);
+        }
+    }
+
+    private void swop(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

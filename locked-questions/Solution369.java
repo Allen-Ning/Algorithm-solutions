991cec4b0dbbaed35d453c20bdbe998a7084cb16
Allen Ning <npc1222@icloud.com>
2020-09-01 08:37:19 +1000

    added solution 369

---
+++ locked-questions/Solution369.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: the first Node
+     * @return: the answer after plus one
+     */
+    public ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        helper(dummy);
+        return dummy.val == 0 ? head : dummy;
+    }
+
+    private int helper(ListNode node) {
+        if (node == null) return 1;
+
+        int value = helper(node.next) + node.val;
+        node.val = value % 10;
+        return value / 10;
+    }
+}

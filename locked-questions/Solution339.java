0c4a87fd38d3124b89fd1f6b161643099f241012
Allen Ning <npc1222@icloud.com>
2020-09-01 07:25:33 +1000

    added solution 339

---
+++ locked-questions/Solution339.java
@@ -0,0 +1,33 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer,
+ *     // rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds,
+ *     // if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds,
+ *     // if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return helper(nestedList, 1);
+    }
+
+    private int helper(List<NestedInteger> nestedList, int step) {
+        int result = 0;
+        for (NestedInteger next : nestedList) {
+            result += next.isInteger() ? next.getInteger() * step : helper(next.getList(), step + 1);
+        }
+        return result;
+    }
+}

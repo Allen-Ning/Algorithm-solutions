75d82b0f46ee846bbb233f246e824389f4291573
Allen Ning <npc1222@icloud.com>
2020-11-16 16:51:15 +1100

    added solution 1603

---
+++ Solution1603.java
@@ -0,0 +1,15 @@
+class ParkingSystem {
+
+    // index big 0, medium 1, small 2
+    int[] slots;
+
+    public ParkingSystem(int big, int medium, int small) {
+        slots = new int[] {big, medium, small};
+    }
+
+    public boolean addCar(int carType) {
+        if (slots[carType - 1] <= 0) return false;
+        slots[carType - 1]--;
+        return true;
+    }
+}

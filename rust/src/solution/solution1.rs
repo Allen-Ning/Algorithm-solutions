70b59e1d8ebea41eaa0cb339b771e0f5314cff6a
Allen Ning <npc1222@icloud.com>
2021-03-27 20:45:20 +1100

    solution1 for rs

---
+++ rust/src/solution/solution1.rs
@@ -0,0 +1,22 @@
+struct Solution {}
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        let mut result = Vec::new(); 
+        for i in 0..nums.len() {
+            let value = target - nums[i];
+            if map.contains_key(&value) {
+                let &index = map.get(&value).expect("key exist");
+                result.push(index);
+                result.push(i as i32);
+                return result;
+            }
+            map.insert( nums[i], i as i32);
+        }
+
+        return result
+    }
+}

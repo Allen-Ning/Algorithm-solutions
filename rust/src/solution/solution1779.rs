bc1591910c44ff1840ac0d464863889c5324e923
Allen Ning <npc1222@icloud.com>
2021-05-07 21:51:53 +1000

    added solution 1779

---
+++ rust/src/solution/solution1779.rs
@@ -0,0 +1,21 @@
+struct Solution {}
+
+impl Solution {
+    pub fn nearest_valid_point(x: i32, y: i32, points: Vec<Vec<i32>>) -> i32 {
+        let mut result = -1;
+        let mut distance = std::i32::MAX;
+        for i in 0..points.len() {
+            let current = &points[i];
+            if current[0] != x && current[1] != y {
+                continue;
+            }
+
+            let current_distance = (current[0] - x).abs() + (current[1] - y).abs();
+            if current_distance < distance {
+               distance = current_distance;
+               result = i as i32;
+            }
+        }
+        return result;
+    }
+}

e8a485e10494a092e7adf2d6d3b0cea107c3ebb3
Allen Ning <npc1222@icloud.com>
2021-05-28 09:54:51 +1000

    changed to solution 170

--- locked-questions/Solution170.java
+++ locked-questions/Solution170.java
@@ -1,18 +1,12 @@
 public class TwoSum {
     Map<Integer, Integer> map = new HashMap();
 
-    /**
-     * @param number: An integer
-     * @return: nothing
-     */
     public void add(int number) {
         map.put(number, map.getOrDefault(number, 0) + 1);
     }
 
-    /**
-     * @param value: An integer
-     * @return: Find if there exists any pair of numbers which sum is equal to the value.
-     */
+
+    // TODO this is not the best solution
     public boolean find(int value) {
         for (int key : map.keySet()) {
             int check = value - key;

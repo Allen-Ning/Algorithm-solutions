adbe8f52c13df9dc2ca7cf02afd4bee714bfc79e
Allen Ning <npc1222@icloud.com>
2021-05-27 10:02:55 +1000

    added solution 1064

--- locked-questions/Solution1064.java
+++ locked-questions/Solution1064.java
@@ -1,9 +1,18 @@
-public class Solution {
-    // please notice this code is not ac
-    public int findMinIndex(int[] data) {
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] == i) return i;
+class Solution {
+    public int fixedPoint(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int result = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < mid) {
+                start = mid + 1;
+            } else {
+                // trick -> return the smallest index i that satisfies arr[i] == i
+                if (arr[mid] == mid) result = mid;
+                end = mid - 1;
+            }
         }
-        return -1;
+        return result;
     }
 }

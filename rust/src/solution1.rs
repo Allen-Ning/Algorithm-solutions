ddcc9b722074b8c41bc2f4c7083ccdfeb45fd366
Allen Ning <npc1222@icloud.com>
2021-09-17 21:02:02 +1000

    clean up for rust solution 1

--- rust/src/solution1.rs
+++ rust/src/solution1.rs
@@ -1,4 +1,3 @@
-
 struct Solution {}
 
 use std::collections::HashMap;
@@ -7,17 +6,22 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::new();
         let mut result = Vec::new();
-        for i in 0..nums.len() {
-            let value = target - nums[i];
-            if map.contains_key(&value) {
-                let &index = map.get(&value).expect("key exist");
-                result.push(index);
-                result.push(i as i32);
-                return result;
-            }
-            map.insert( nums[i], i as i32);
-        }
 
-        return result
+        let length = nums.len();
+        for i in 0..length {
+            let num = nums[i];
+            let lookup_num = target - num;
+            match map.get(&lookup_num) {
+                None => {
+                    map.insert(num, i as i32);
+                }
+                Some(index) => {
+                    result.push(*index);
+                    result.push(i as i32);
+                    return result;
+                }
+            }
+        }
+        return result;
     }
 }

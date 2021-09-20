73cb3ba5d75e929e0186f9690257d41e5931266a
Allen Ning <npc1222@icloud.com>
2021-09-20 23:04:36 +1000

    cleanup main

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,9 @@
-mod solution1;
-mod solution1779;
-mod solution1773;
-mod solution1790;
+// mod solution1;
+// mod solution21;
+// mod solution876;
+// mod solution1773;
+// mod solution1779;
+// mod solution1790;
 mod common;
 
 fn main() {

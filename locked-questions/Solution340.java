80c7d44711cc439367ab0d349f619dd5c5e5ada5
Allen Ning <npc1222@icloud.com>
2022-01-20 00:09:51 +1100

    refactored java solution 340

--- locked-questions/Solution340.java
+++ locked-questions/Solution340.java
@@ -1,19 +1,22 @@
-public class Solution {
+class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        int start = 0;
-        int end = 0;
-        int total = 0;
-        int[] letters = new int[256];
         int result = 0;
-        int length = s.length();
-        while (end < length) {
-            if (letters[s.charAt(end)]++ == 0) total += 1;
-
-            while (end >= k && total > k && start <= end) {
-                if (--letters[s.charAt(start++)] == 0) total -= 1;
+        int[] check = new int[256];
+        int counter = 0;
+        int start = 0;
+        // trick -> classic sliding windown question:
+        //          1. move the end pointer as much as the sliding window can until it's more than k
+        //          2. move the start pointer to meet the requirement -sliding window at most k
+        //          3. record the best lenght between start pointer (included) and end pointer (included)
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (check[c]++ == 0) counter++;
+            while (counter > k) {
+                char n = s.charAt(start);
+                if (--check[n] == 0) counter--;
+                start++;
             }
             result = Math.max(result, end - start + 1);
-            end++;
         }
         return result;
     }

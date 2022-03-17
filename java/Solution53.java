3aa0edcde3d78ccafed9edc324cf39c3c840dfef
Allen Ning <npc1222@icloud.com>
2022-03-18 09:54:30 +1100

    added polished java solution 53

--- java/Solution53.java
+++ java/Solution53.java
@@ -1,14 +1,12 @@
 class Solution {
-  public int maxSubArray(int[] nums) {
-    if(nums.length == 0) return 0;
-    if(nums.length ==1) return nums[0];
-    int[] sum = new int[nums.length];
-    sum[0] = nums[0];
-    int maxSum = sum[0];
-    for (int i = 1; i < nums.length; i++) {
-      sum[i] = Math.max(nums[i], nums[i] + sum[i - 1]);
-      if (sum[i] > maxSum) maxSum = sum[i];
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int result = max;
+        for (int i = 1; i < nums.length; i++) {
+            // trick -> max means the maximum value -> all subarrays ending with index i
+            max = Math.max(nums[i], nums[i] + max);
+            result = Math.max(result, max);
+        }
+        return result;
     }
-    return maxSum;
-  }
 }

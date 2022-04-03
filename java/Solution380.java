22207aaceac428c228eaf5ccf32e50eb3e7f74f7
Allen Ning <npc1222@icloud.com>
2022-04-03 22:25:18 +1000

    added polished java solution 380

--- java/Solution380.java
+++ java/Solution380.java
@@ -1,37 +1,36 @@
 class RandomizedSet {
-    HashMap<Integer, Integer> map;
-    ArrayList<Integer> list;
-    Random random;
 
-    /** Initialize your data structure here. */
+    HashMap<Integer, Integer> map;
+    List<Integer> list;
+    Random random; 
+
     public RandomizedSet() {
         map = new HashMap();
         list = new ArrayList();
         random = new Random();
     }
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
         if (map.containsKey(val)) return false;
         list.add(val);
         map.put(val, list.size() - 1);
         return true;
     }
-
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    
     public boolean remove(int val) {
         if (!map.containsKey(val)) return false;
-        int index = map.get(val);
-        int lastVal = list.get(list.size() - 1);
-        int lastValIndex = map.get(lastVal);
-        list.set(index, lastVal);
-        list.remove(list.size() - 1);
-        map.put(lastVal, index);
+
+        Integer index = map.get(val);
+        int last = list.get(list.size() - 1);
+        list.set(index, last);
+        // trick -> we need to update the last element index
+        map.put(last, index);
         map.remove(val);
+        // trick -> forget api how to remove last in arraylist
+        list.remove(list.size() - 1);
         return true;
     }
-
-    /** Get a random element from the set. */
+    
     public int getRandom() {
         int index = random.nextInt(list.size());
         return list.get(index);

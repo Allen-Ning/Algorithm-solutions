bcad8214208b088a25f1eb569034f73c48e970df
Allen Ning <npc1222@icloud.com>
2022-05-17 22:05:59 +1000

    added updated solution 70

--- java/Solution70.java
+++ java/Solution70.java
@@ -1,16 +1,12 @@
 class Solution {
-  public int climbStairs(int n) {
-    if (n == 1) return 1;
-    if (n == 2) return 2;
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-    int[] result = new int[n + 1];
-    result[0] = 0;
-    result[1] = 1;
-    result[2] = 2;
-
-    for (int i = 3; i < n + 1; i++) {
-      result[i] = result[i -1] + result[i - 2];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i -2];          
+        }
+        return dp[n];
     }
-    return result[n];
-  }
 }

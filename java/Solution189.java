625de5c90459494a7e067945989411e20fed7fbc
Allen Ning <npc1222@icloud.com>
2022-05-24 20:47:58 +1000

    polished java solution 189

--- java/Solution189.java
+++ java/Solution189.java
@@ -1,27 +1,26 @@
-// [1,2,3,4,5,6,7] 
-// [7, 6, 5, 4, 3, 2, 1] 
-// [7, 6, 5, 4, 3, 2, 1]
-// [5, 6, 7, 4, 3, 2, 1]
-// [5, 6, 7, 1, 2, 3, 4]
-// please notice k %= nums.length
 class Solution {
-
     public void rotate(int[] nums, int k) {
-        if (nums == null || nums.length <= 1 || k == 0) return;
-        k %= nums.length;
+        // trick -> if move nums.length step, it will be back to the original state
+        k = k % nums.length;
+        // e.g
+        // [1, 2, 3, 4, 5, 6, 7]
+        // [7, 6, 5, 4, 3, 2, 1]
+        // [7, 6, 5 | 4, 3, 2, 1]
+        // [5, 6, 7 | 1, 2, 3, 4]
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length -1);
-        return;
+        reverse(nums, k, nums.length - 1);
     }
 
-    private void reverse(int[] nums, int i, int j) {
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+    private void reverse(int nums[], int start, int end) {
+        int l = start;
+        int h = end;
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }

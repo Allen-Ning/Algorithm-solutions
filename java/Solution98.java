4c8eb580ba39429abe981de81cd41380e750f6a5
Allen Ning <npc1222@icloud.com>
2022-05-26 09:02:11 +1000

    added polished java solution 98

--- java/Solution98.java
+++ java/Solution98.java
@@ -4,17 +4,25 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-      helper(node, null, null);
+        // trick -> use long to avoid stack overflow. E.g. [2147483647]
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private boolean helper(TreeNode node, int min, int max) {
-      if (node == null) return true;
-      if ((max != null && node.val >= max ) || (min != null && node.val <= min)) return false;
-      return helper(node.left, min, node.val) && helper(node.right, node.val, min);
+    private boolean helper(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+
+        return helper(node.left, min, node.val) && helper(node.right, node.val, max);
     }
 }

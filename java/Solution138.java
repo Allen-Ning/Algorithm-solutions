590edc912bbfcb9eb3f39c2747d6fd93d7b9faeb
Allen Ning <npc1222@icloud.com>
2022-05-26 22:27:41 +1000

    added recursive java solution 138

--- java/Solution138.java
+++ java/Solution138.java
@@ -1,60 +1,57 @@
 /*
 // Definition for a Node.
 class Node {
-    public int val;
-    public Node next;
-    public Node random;
+    int val;
+    Node next;
+    Node random;
 
-    public Node() {}
-
-    public Node(int _val,Node _next,Node _random) {
-        val = _val;
-        next = _next;
-        random = _random;
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
-};
+}
 */
 class Solution {
-    
-    // 1 -> 1' -> 2 -> 2'
     public Node copyRandomList(Node head) {
-        if (head == null) return head;
-        Node node = head;
+        if (head == null) return null;
 
-        // build A->A'->B->B'->C->C'->D->D'
-        while (node != null) {
-            Node nextNode = node.next;
-            Node cpNode = new Node(node.val, null, null);
-
-            node.next = cpNode;
-            cpNode.next = nextNode;
-    
-            node = nextNode;
-        }
-        
-        // assign random pointer
-        node = head;
-        while (node != null) {
-            if (node.random != null) node.next.random = node.random.next;
-            node = node.next.next;
-        }
-        
-        // split linked list
-        // 1 -> 1' -> 2 -> 2'
-        node = head;
-        Node newHead = head.next;
-        Node cur = newHead;
-        while (node != null && node.next.next != null) {
-            Node nextNode = node.next.next;
-            cur.next = nextNode.next;
-            node.next = nextNode;
-            
-            cur = nextNode.next;
-            node = nextNode;
-        }
-        node.next = null;
-        return newHead;
-        
+        clone(head);
+        assignRandom(head);
+        Node copyHead = head.next;
+        splitNode(head);
+        return copyHead;
     }
-    
+
+    private void clone(Node node) {
+        if (node == null) return;
+
+        Node next = node.next;
+        Node copy = new Node(node.val);
+        node.next = copy;
+        copy.next = next;
+        clone(next);
+    }
+
+    private void assignRandom(Node node) {
+        if (node == null) return;
+
+        Node next = node.next.next;
+        Node copy = node.next;
+        if (node.random != null) copy.random = node.random.next;
+        assignRandom(next);
+    }
+
+    // node copy  next
+    // A -> A' -> B -> B' 
+    private void splitNode(Node node) {
+        if (node == null) return;
+
+        Node next = node.next.next;
+        Node copy = node.next;
+        node.next = next;
+
+        if (next != null) copy.next = next.next;
+        splitNode(next);
+    }
 }

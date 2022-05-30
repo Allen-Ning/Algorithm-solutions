cf8885391dccec67115926ff26fc39b2a5a89437
Allen Ning <npc1222@icloud.com>
2022-05-30 20:09:51 +1000

    added polished java solution 543

--- java/Solution543.java
+++ java/Solution543.java
@@ -4,23 +4,28 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-  int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] result = new int[]{ 0 };
+        helper(root, result);
+        return result[0];
+    }
 
-  public int diameterOfBinaryTree(TreeNode root) {
-    helper(root);
-    return max;
-  }
+    private int helper(TreeNode node, int[] result) {
+        if (node == null) return 0;
 
-  private int helper(TreeNode node) {
-    if (node == null) return 0;
-
-    int left = helper(node.left);
-    int right = helper(node.right);
-    max = Math.max(max, left + right);
-    return Math.max(left, right) + 1;
-  }
+        int left = helper(node.left, result);
+        int right = helper(node.right, result);
+        result[0] = Math.max(result[0], left + right);
+        return Math.max(left, right) + 1;
+    }
 }

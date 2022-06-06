4f1953e4da3b5b2c73aad3ed6d4398fdddbc1b8e
Allen Ning <npc1222@icloud.com>
2022-06-06 19:56:31 +1000

    added polished java solution 338

--- java/Solution338.java
+++ java/Solution338.java
@@ -1,10 +1,23 @@
 class Solution {
-  public int[] countBits(int num) {
-    int[] result = new int[num + 1];
-    result[0] = 0;
-    for (int i = 1; i < result.length; i++) {
-      result[i] = result[i >> 1] + (i & 1);
+    public int[] countBits(int n) {
+        // trick -> find example below
+        // 0 --> 0
+        // 1 --> 1
+        // 2 --> 10
+        // 3 --> 11
+        // 4 --> 100
+        // 5 --> 101
+        // 6 --> 110
+        // 7 --> 111
+        // 8 --> 1000
+        int[] result = new int[n + 1];
+        for (int i = 1; i < result.length; i++) {
+            if (i % 2 == 0) {
+                result[i] = result[i / 2];
+            } else {
+                result[i] = result[i / 2] + 1;
+            }
+        }
+        return result;
     }
-    return result;
-  }
 }

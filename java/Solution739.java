30972914b565e67f90b22493c5ccb61de541313d
Allen Ning <npc1222@icloud.com>
2022-06-14 23:34:57 +1000

    added polished java solution for 739

--- java/Solution739.java
+++ java/Solution739.java
@@ -1,27 +1,24 @@
 class Solution {
-    //  0    1   2 . 3 . 4 . 5  6 .  7
-    // [73, 74, 75, 71, 69, 72, 76, 73]
-    //  1   1   ?    ?   1   1   ?   0
-    public int[] dailyTemperatures(int[] T) {
-        if (T == null || T.length == 0) return null;
-        int[] results = new int[T.length];
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
         Stack<Integer> stack = new Stack();
-        for (int i = 0; i < T.length; i++) {
-            while (!stack.isEmpty()) {
-                int topIndex = stack.peek();
-                if (T[i] > T[topIndex]) {
-                    results[topIndex] = i - topIndex;
-                    stack.pop();
-                } else {
-                    break;
-                }
-            }
-            if (i < (T.length - 1) && T[i] < T[i + 1]) {
-                results[i] = 1;
-            } else {
+        for (int i = 0; i < temperatures.length; i++) {
+            int temperature = temperatures[i];
+            if (stack.isEmpty()) {
                 stack.push(i);
+                continue;
             }
+
+            int prevValue = temperatures[stack.peek()];
+            while (!stack.isEmpty() && prevValue < temperature) {
+                int resultIndex = stack.pop();
+                result[resultIndex] = i - resultIndex;
+
+                if (stack.isEmpty()) break;
+                prevValue = temperatures[stack.peek()];
+            }
+            stack.push(i);
         }
-        return results;
+        return result;
     }
 }

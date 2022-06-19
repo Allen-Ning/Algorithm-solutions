8fbe0f0e3b16d78d9669b1f4ccda8a891c7aa5bc
Allen Ning <npc1222@icloud.com>
2022-06-19 20:21:38 +1000

    added a better version for java solution 128

--- java/Solution128.java
+++ java/Solution128.java
@@ -1,19 +1,23 @@
 class Solution {
-  public int longestConsecutive(int[] nums) {
-    if (nums.length == 0) return 0;
-    HashMap<Integer, Integer> values = new HashMap();
-    for (int num : nums) values.put(num, 1);
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for (int num : nums) set.add(num);
 
-    int result = 0;
-    for (int num : nums) {
-      if (values.containsKey(num - 1)) continue;
+        int result = 0;
+        for (int num : set) {
+            if (set.contains(num - 1)) continue;    
 
-      int nextNum = num;
-      while (values.containsKey(nextNum)) {
-        nextNum++;
-      }
-      result = Math.max(result, nextNum - num);
+            int low = num;
+            int high = num;
+            // trick -> if there is duplicated num, only count 1 time
+            //         e.g. input: [1, 2, 2, 3, 4, 5]
+            //              output: 5 [1, 2, 3, 4, 5]
+            while (set.contains(high + 1)) {
+                high += 1;
+            }
+
+            result = Math.max(result, high - low + 1);
+        }
+        return result;
     }
-    return result;
-  }
 }

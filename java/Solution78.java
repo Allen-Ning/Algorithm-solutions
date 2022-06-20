3afb24eed775f85c8d6979356c81264e38b4ccbc
Allen Ning <npc1222@icloud.com>
2022-06-20 21:07:55 +1000

    added a better version of java solution 78

--- java/Solution78.java
+++ java/Solution78.java
@@ -1,28 +1,20 @@
-import java.util.List;
-import java.util.ArrayList;
-
-public class Solution78 {
-
-    private int[] nums;
-    private List<List<Integer>> list;
+class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        this.nums = nums;
-        this.list = new ArrayList();
-        List<Integer> result = new ArrayList();
-        list.add(new ArrayList());
-        helper(result, 0);
-        return list;
+        List<List<Integer>> results = new ArrayList();
+        helper(nums, 0, results, new ArrayList());
+        return results;
     }
 
-    private void helper(List<Integer> result, int start) {
-        if (result.size() == this.nums.length) {
-            return;
-        }
+    private void helper(int[] nums, int index, List<List<Integer>> results, List<Integer> result) {
+        List<Integer> clone = new ArrayList();
+        for (Integer num : result) clone.add(num);
+        results.add(clone);
 
-        for (int i = start; i < nums.length; i++) {
-            result.add(this.nums[i]);
-            list.add( new ArrayList(result));
-            helper(result, i + 1);
+        if (index >= nums.length) return;
+
+        for (int i = index; i < nums.length; i++) {
+            result.add(nums[i]);
+            helper(nums, i + 1, results, result);
             result.remove(result.size() - 1);
         }
     }

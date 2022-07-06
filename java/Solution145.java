8665d8046ef962ad861e8a9b49a4a9e9baa0554a
Allen Ning <npc1222@icloud.com>
2022-07-06 23:03:55 +1000

    polished java iterative approach for postorder following the same template

--- java/Solution145.java
+++ java/Solution145.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList();
+        List<Integer> result = new LinkedList();
         Stack<TreeNode> stack = new Stack();
 
         TreeNode node = root;
@@ -23,16 +23,16 @@ class Solution {
 
         while (node != null || !stack.isEmpty()) {
             while (node != null) {
-                result.add(node.val);
+                // trick -> add to the head of the linkedlist
+                result.add(0, node.val);
                 stack.add(node);
+                // trick -> visit the right child of the node first
                 node = node.right;
             }
 
             node = stack.pop();
             node = node.left;
         }
-
-        Collections.reverse(result);
         return result;
     }
 }

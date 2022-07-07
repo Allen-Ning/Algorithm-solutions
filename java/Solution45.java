decb05d98d38e4c466a337ed3e8f9e0aa975285a
Allen Ning <npc1222@icloud.com>
2022-07-07 21:18:10 +1000

    added a polished version of solution 45

--- java/Solution45.java
+++ java/Solution45.java
@@ -1,18 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        int[] dp = new int[nums.length];
-        dp[nums.length - 1] = 0;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            int min = Integer.MAX_VALUE;
-            int maxReachIndex = Math.min(i + nums[i] + 1, nums.length);
-            for (int j = i + 1; j < maxReachIndex; j++) {
-                if (dp[j] < 0) continue; 
-                min = Math.min(min, dp[j]);
-            }
+        int step = 0;
+        int prevEnd = nums[0];
+        int newEnd = 0;
 
-            dp[i] = (min == Integer.MAX_VALUE ? -1 : min + 1);
+        for (int i = 1; i < nums.length; i++) {
+            if (prevEnd >= nums.length - 1) return step + 1;
+
+            newEnd = Math.max(newEnd, nums[i] + i);
+            if (i < prevEnd) continue;
+
+            prevEnd = newEnd;
+            step++;
         }
-        return dp[0];
+        return step;
     }
 }

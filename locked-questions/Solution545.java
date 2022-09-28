b98f60f83dc40dd8aa821e82896d519ae3cfd9de
Allen Ning <npc1222@icloud.com>
2022-09-28 22:05:27 +1000

    added polished java solution 545

--- locked-questions/Solution545.java
+++ locked-questions/Solution545.java
@@ -1,60 +1,61 @@
 /**
- * Definition of TreeNode:
+ * Definition for a binary tree node.
  * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
- *         this.left = this.right = null;
+ *         this.left = left;
+ *         this.right = right;
  *     }
  * }
  */
-
-public class Solution {
+class Solution {
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
-        List<Integer> lists = new ArrayList();
-        if (root == null) return lists;
-
-        lists.add(root.val);
-        leftBoundary(root.left, lists);
-        bottomBoundary(root.left, lists);
-        bottomBoundary(root.right, lists);
-        rightBoundary(root.right, lists);
-        return lists;
+        List<Integer> results = new ArrayList();
+        results.add(root.val);
+        addLeft(root.left, results);
+        // trick -> corner cases
+        //          addLeaf could potentially add root again
+        //          if not split into addLeaf(root.left) and addLeaf(root.right)
+        addLeaf(root.left, results);
+        addLeaf(root.right, results);
+        addRight(root.right, results);
+        return results;
     }
 
-    private void leftBoundary(TreeNode node, List<Integer> lists) {
+    private void addLeft(TreeNode node, List<Integer> results) {
         if (node == null) return;
+        if (node.left == null && node.right == null) return;
 
+        results.add(node.val);
         if (node.left != null) {
-            lists.add(node.val);
-            leftBoundary(node.left, lists);
+            addLeft(node.left, results);
         } else if (node.right != null) {
-            lists.add(node.val);
-            leftBoundary(node.right, lists);
+            addLeft(node.right, results);
         }
     }
 
-    private void rightBoundary(TreeNode node, List<Integer> lists) {
+    private void addLeaf(TreeNode node, List<Integer> results) {
         if (node == null) return;
+        if (node.left == null && node.right == null) results.add(node.val);
 
-        // trick -> this is very smart move
-        //          in order to get bottom up, and need to call rightBoundary() first and
-        //          add node.val into lists during returning process
+        addLeaf(node.left, results);
+        addLeaf(node.right, results);
+    }
+
+      private void addRight(TreeNode node, List<Integer> results) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) return;
+
         if (node.right != null) {
-            rightBoundary(node.right, lists);
-            lists.add(node.val);
+            addRight(node.right, results);
         } else if (node.left != null) {
-            rightBoundary(node.left, lists);
-            lists.add(node.val);
+            addRight(node.left, results);
         }
-    }
-
-    private void bottomBoundary(TreeNode node, List<Integer> lists) {
-        if (node == null) return;
-
-        if (node.left == null && node.right == null) lists.add(node.val);
-        bottomBoundary(node.left, lists);
-        bottomBoundary(node.right, lists);
+        results.add(node.val);
     }
 }

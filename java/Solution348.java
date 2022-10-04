4709edfb884098ecc9e47227ff7bc5e0fd6ba963
Allen Ning <npc1222@icloud.com>
2022-10-04 20:39:12 +1100

    added java solution 348

--- java/Solution348.java
+++ java/Solution348.java
@@ -1,39 +1,31 @@
 class TicTacToe {
-
     int[] rows;
-    int[] cols;
-    int diagonal;
+    int[] columns;
+    int leftDiagonal;
     int antiDiagonal;
     int n;
 
     public TicTacToe(int n) {
-        this.n = n;
         this.rows = new int[n];
-        this.cols = new int[n];
+        this.columns = new int[n];
+        this.leftDiagonal = 0;
+        this.antiDiagonal = 0;
+        this.n = n;
     }
 
     public int move(int row, int col, int player) {
-        int addedValue = (player == 1 ? 1 : -1);
-        this.rows[row] += addedValue;
-        this.cols[col] += addedValue;
-        if (row == col) diagonal += addedValue;
+        int addedValue = player == 1 ? 1 : -1;
+
+        rows[row] += addedValue;
+        columns[col] += addedValue;
+        if (row == col) leftDiagonal += addedValue;
+        // trick -> this is the trick how to calculate antiDiagonal
         if (row + col == n - 1) antiDiagonal += addedValue;
 
-        if (rows[row] == n || 
-            cols[col] == n ||
-            diagonal == n ||
-            antiDiagonal == n
-        ) {
-            return 1;
-        } else if (rows[row] == -1 * n ||
-                   cols[col] == -1 * n ||
-                   diagonal == -1 * n ||
-                   antiDiagonal == -1 * n
-        ) {
-            return 2;
-        } else {
-            return 0;
-        }
+        if (rows[row] == n || columns[col] == n || leftDiagonal == n || antiDiagonal == n) return 1;
+        if (rows[row] == -n || columns[col] == -n || leftDiagonal == -n || antiDiagonal == -n) return 2;
+
+        return 0;
     }
 }
 

d95be3b023aec58bda3104deca6377d7995c38ed
Allen Ning <npc1222@icloud.com>
2022-10-06 22:30:33 +1100

    added java polished solution 632

--- java/Solution632.java
+++ java/Solution632.java
@@ -1,41 +1,35 @@
 class Solution {
-    public int[] smallestRange(List<List<Integer>> nums) {
-      int[] result = new int[2];
-      if (nums == null || nums.size() == 0) return result;
+    public int[] smallestRange(List<List<Integer>> lists) {
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((num1, num2) -> num1[0] - num2[0]);
 
-      int size = nums.size();
-      int[] indexes = new int[nums.size()];
-      PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-
-      int max = 0;
-      for (int i = 0; i < indexes.length; i++) {
-          queue.offer(new int[] {i, nums.get(i).get(indexes[0])});
-          max = Math.max(max, nums.get(i).get(indexes[0]));
-      }
-
-      int range = max - queue.peek()[1];
-      result = new int[] {queue.peek()[1], max};
-      int counter = 1;
-      while (queue.size() > 0) {
-        int[] element = queue.poll();
-        int min = element[1];
-        if (max - min < range) {
-            result = new int[] {min, max};
-            range = max - min;
+        int[] indexes = new int[lists.size()];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < lists.size(); i++) {
+            int value = lists.get(i).get(0);
+            max = Math.max(max, value);
+            minHeap.add(new int[] {value, i});
         }
+        int[] result = new int[]{minHeap.peek()[0], max};
 
-        int rowNum = element[0];
-        indexes[rowNum] += 1;
+        while (minHeap.size() > 0) {
+            int[] element = minHeap.poll();
+            int min = element[0];
+            int i = element[1];
 
-        // trick -> we need to be careful about this if any of the array reaching ends
-        // we don't need to do any check further cos we always remove the smallest one
-        if (indexes[rowNum] >= nums.get(rowNum).size()) break;
-        int newValue = nums.get(rowNum).get(indexes[rowNum]);
+            if (max - min < result[1] - result[0] || (((max - min) == (result[1] - result[0])) && min < result[0])) {
+                result[0] = min;
+                result[1] = max;
+            }
 
-        queue.offer(new int[] {rowNum, newValue});
-        max = Math.max(max, newValue);
-        counter++;
-      }
-      return result;
+            indexes[i] += 1;
+            // trick -> we need to be careful about this if any of the array reaching ends
+            // we don't need to do any check further cos we always remove the smallest one
+            if (indexes[i] >= lists.get(i).size()) break;
+
+            int nextValue = lists.get(i).get(indexes[i]);
+            minHeap.add(new int[]{nextValue, i});
+            max = Math.max(max, nextValue);
+        }
+        return result;
     }
 }

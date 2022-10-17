f597adf274d69dc3a31b10066e3dd8c75977f3fe
Allen Ning <npc1222@icloud.com>
2022-10-18 09:13:12 +1100

    added java polished solution 103

--- java/Solution103.java
+++ java/Solution103.java
@@ -8,28 +8,32 @@
  * }
  */
 class Solution {
-  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-    LinkedList<TreeNode> queue = new LinkedList();
-    ArrayList<List<Integer>> result = new ArrayList();
-    if (root == null) return result;
-    queue.add(root);
-    int level = 0;
-    while (queue.size() > 0) {
-      int size = queue.size();
-      ArrayList<Integer> levelList = new ArrayList();
-      for (int i = 0; i < size; i++) {
-        TreeNode node = queue.pop();
-        if (level % 2 == 0) {
-          levelList.add(node.val);
-        } else {
-          levelList.add(0, node.val);
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList();
+        ArrayList<List<Integer>> result = new ArrayList();
+        if (root == null) return result;
+
+        queue.add(root);
+        int level = 0;
+        while (queue.size() > 0) {
+            int size = queue.size();
+            LinkedList<Integer> levelList = new LinkedList();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                // trick -> linkedlist addFirst, addLast is o(1)
+                if (level % 2 == 0) {
+                    levelList.addLast(node.val);
+                } else {
+                    levelList.addFirst(node.val);
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+             }
+          level += 1;
+          result.add(levelList);
         }
-        if (node.left != null) queue.offer(node.left);
-        if (node.right != null) queue.offer(node.right);
-      }
-      level += 1;
-      result.add(levelList);
+        return result;
     }
-    return result;
-  }
 }

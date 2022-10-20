028940e14df00d6a1e4cf4bc447753bb68dacd3e
Allen Ning <npc1222@icloud.com>
2022-10-21 00:12:05 +1100

    added polished java solution 875

--- java/Solution875.java
+++ java/Solution875.java
@@ -1,38 +1,30 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int H) {
-        if (piles == null || piles.length == 0 || H == 0) return 0;
-
-        int max = 0;
-        for (int pile : piles) max = Math.max(max, pile);
-
-        if (piles.length == H) {
-            return max;
-        } else if (piles.length > H) {
-            return -1;
-        } else {
-            int l = 1;
-            int r = max;
-            while (l < r) {
-                int mid = l + (r - l) / 2;
-                if (eatHours(piles, mid) > H) {
-                    l = mid + 1;
-                } else {
-                    r = mid;
-                }
-            }
-            return l;
+    public int minEatingSpeed(int[] piles, int h) {
+        int start = 0;
+        int end = piles[0];
+        for (int i = 1; i < piles.length; i++) {
+            end = Math.max(piles[i], end);
         }
-    }
+        end += 1;
 
-    private int eatHours(int[] piles, int eatSpeed) {
-        int hour = 0;
-        for (int pile : piles) {
-            if (pile <= eatSpeed) {
-                hour += 1;
+        while (start + 1 != end) {
+            int guess = start + (end - start) / 2;
+
+            long hours = 0;
+            for (int i = 0; i < piles.length; i++) {
+                long each = piles[i] / guess;
+                if (piles[i] % guess != 0) each += 1;
+                hours += each;
+            }
+
+            // start                 end
+            // 3, 4, 5, 6, 7 , 8, 8  xx
+            if (hours > (long)h) {
+                start = guess;
             } else {
-                hour += (pile / eatSpeed + 1);
+                end = guess;
             }
         }
-        return hour;
+        return end;
     }
 }

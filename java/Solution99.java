9b9e80f06e35f7ed705cb295b05610be13796856
Allen Ning <npc1222@icloud.com>
2022-10-23 18:06:00 +1100

    added java solution 99

--- java/Solution99.java
+++ java/Solution99.java
@@ -1,3 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 // 1 2 3 4 5 6 7 - good example 1
 
 // 1 2 4 3 5 6 7 - bad exmaple 1
@@ -5,45 +21,35 @@
 // 7 2 3 4 5 6 1 - bad exmaple 3
 
 // Summary - binary tree inorder travel should  return ascending values (e.g. good example 1)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
+    public void recoverTree(TreeNode root) {
+        List<TreeNode> result = new ArrayList();
+        TreeNode[] prev = new TreeNode[] { null };
 
-  TreeNode first = null;
-  TreeNode second = null;
-  TreeNode prev = null;
-  public void recoverTree(TreeNode root) {
-    helper(root);
-    swop(first, second);
-  }
-
-  private void helper(TreeNode node) {
-    if (node == null) return;
-
-    helper(node.left);
-
-    if (first == null && prev != null && prev.val >= node.val) {
-      first = prev;
+        helper(root, result, prev);
+        swop(result.get(0), result.get(1));
     }
 
-    if (first != null && prev != null && prev.val >= node.val) {
-      second = node;
+    private void helper(TreeNode node, List<TreeNode> result, TreeNode[] prev) {
+        if (node == null) return;
+
+        helper(node.left, result, prev);
+
+        if (prev[0] != null && node.val < prev[0].val) {
+            if (result.size() == 0) {
+                result.add(prev[0]);
+                result.add(node);
+            } else {
+                result.set(1,node);
+            }
+        }
+        prev[0] = node;
+        helper(node.right, result, prev);
     }
-    prev = node;
 
-    helper(node.right);
-  }
-
-  private void swop(TreeNode node1, TreeNode node2) {
-    int temp = node1.val;
-    node1.val = node2.val;
-    node2.val = temp;
-  }
+    private void swop(TreeNode node1, TreeNode node2) {
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+    }
 }

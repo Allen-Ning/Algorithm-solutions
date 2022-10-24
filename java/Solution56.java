d3eccf3f8dde6f8eda785fb27bda8854b75f84a1
Allen Ning <npc1222@icloud.com>
2022-10-24 22:01:51 +1100

    added polished java solution 51

--- java/Solution56.java
+++ java/Solution56.java
@@ -1,31 +1,27 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
-        List<Interval> list = new ArrayList();
-        if(intervals == null || intervals.size() == 0) return list;
-        intervals.sort((a,b) -> Integer.compare(a.start, b.start));
-         int min = intervals.get(0).start;
-        int max = intervals.get(0).end;
-        for (int i = 1; i < intervals.size(); i++) {
-          int currentMin = intervals.get(i).start;
-          int currentMax = intervals.get(i).end;
-          if (currentMin <= max) {
-            max = Math.max(currentMax, max);
-          } else {
-            list.add(new Interval(min, max));
-            min = currentMin;
-            max = currentMax;
-          }
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (e1, e2) -> e1[0] - e2[0]);
+
+        ArrayList<int[]> list = new ArrayList();
+        int[] prev = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] current = intervals[i];
+
+            if (prev[1] < current[0]) {
+                list.add(prev);
+                prev = current;
+            } else {
+                // trick -> we need max here for the end
+                prev[1] = Math.max(prev[1], current[1]);
+            }
         }
-        list.add(new Interval(min, max));
-        return list;
+
+        // trick -> easy to forget to add the last element
+        list.add(prev);
+        int[][] results = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            results[i] = list.get(i);
+        }
+        return results;
     }
 }

a1d8d478b3ac0839279103fed91071fdb9484e27
Allen Ning <npc1222@icloud.com>
2022-10-30 13:55:40 +1100

    added polished java solution 227

--- java/Solution227.java
+++ java/Solution227.java
@@ -1,5 +1,6 @@
 class Solution {
   public int calculate(String s) {
+    // trick -> sign is actually prev-sign
     char sign = '+';
     int num = 0;
     Stack<Integer> stack = new Stack();
@@ -8,20 +9,18 @@ class Solution {
       char c = modifiedS.charAt(i);
       if (c == ' ') continue;
 
-      if ('0' <= c && c <= '9') {
-        num = num * 10 + (c - '0');
-      }
+      if ('0' <= c && c <= '9') num = num * 10 + (c - '0');
+
+      // trick -> not do forget to trigger this when string reaching the end
       if (c > '9' || c < '0' || (i == modifiedS.length() -1)){
         if (sign == '+') {
           stack.push(num);
         } else if (sign == '-') {
           stack.push(-num);
         } else if (sign == '*') {
-          int val = stack.pop() * num;
-          stack.push(val);
+           stack.push(stack.pop() * num);
         } else if (sign == '/') {
-          int val = stack.pop() / num;
-          stack.push(val);
+          stack.push(stack.pop() / num);
         }
         sign = c;
         num = 0;

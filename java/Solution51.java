728772dc6d9b8ccdb926f76761735568f7b4f767
Allen Ning <npc1222@icloud.com>
2022-11-20 23:11:55 +1100

    added polished java solution 51

--- java/Solution51.java
+++ java/Solution51.java
@@ -1,58 +1,65 @@
 class Solution {
-  public List<List<String>> solveNQueens(int n) {
-    List<List<String>> results = new ArrayList();
-    helper(new int[n], 0, results);
-    return results;
-  }
-
-  private void helper(int[] queens, int current, List<List<String>> results) {
-    if (current >= queens.length) {
-      List<String> result = generateResult(queens);
-      results.add(result);
-      return;
-    }
-
-    for (int i = 0; i < queens.length; i++) {
-      if (!canPlace(queens, current, i)) continue;
-      queens[current] = i;
-      helper(queens, current + 1, results);
-    }
-  }
-
-  private boolean canPlace(int[] queens, int current, int position) {
-    int total = current + 1;
-    int currentX = current - 1;
-    int currentY = position;
-
-    // check leftTop
-    int currentLeftY = position;
-    // check rirghtTop
-    int currentRightY = position;
-
-    while (total > 0) {
-      if (currentX < 0) return true;
-      if (queens[currentX] == --currentLeftY ||
-          queens[currentX] == currentY ||
-          queens[currentX] == ++currentRightY) return false;
-        total--;
-        currentX--;
-    }
-    return true;
-  }
-
-  private List<String> generateResult(int[] queens) {
-    List<String> list = new ArrayList();
-    for (int i = 0; i < queens.length; i++) {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> results = new ArrayList();
+        boolean[][] isVisited = new boolean[n][n];
+        List<String> result = new ArrayList();
         StringBuilder sb = new StringBuilder();
-        for (int j = 0; j < queens.length; j++) {
-            if (queens[i] == j) {
-                sb.append('Q');
-            } else {
-                sb.append('.');
-            }
-        }
-        list.add(sb.toString());
+        for (int i = 0; i < n; i++) sb.append(".");
+        for (int i = 0; i < n; i++) result.add(sb.toString());
+
+        helper(results, result, isVisited, 0, n);
+        return results;
     }
-    return list;
-  }
+    
+    private void helper(List<List<String>> results, 
+                        List<String> result, 
+                        boolean[][] isVisited, 
+                        int row, 
+                        int n
+    ) {
+        if (row >= n) {
+            List<String> finalResult = new ArrayList(result);
+            results.add(finalResult);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (!isValid(isVisited, row, col)) continue;
+            isVisited[row][col] = true;
+            StringBuilder sb = new StringBuilder(result.get(row));
+            sb.setCharAt(col, 'Q');
+            result.set(row, sb.toString());
+            helper(results, result, isVisited, row + 1, n);
+            sb.setCharAt(col, '.');
+            result.set(row, sb.toString());
+            isVisited[row][col] = false;
+        }
+    }
+
+    private boolean isValid(boolean[][] isVisited, int row, int col) {
+        int n = isVisited[0].length;
+        // trick - no need to check left right cos we only add a queen per row
+        //         no need to check bottom left and bottom right cos there is no queens added
+        // check up down
+        for (int i = 0; i < n; i++) {
+            if (isVisited[i][col] && i != row) return false; 
+        }
+
+        int i = row;
+        int j = col;
+
+        // check top right
+        while (i >= 0 && i < n && j >= 0 && j < n) {
+            if (isVisited[i--][j++] && i != row && j != col) return false;
+        }
+
+        i = row;
+        j = col;
+
+        // check top left
+        while (i >= 0 && i < n && j >= 0 && j < n) {
+            if (isVisited[i--][j--] && i != row && j != col) return false;
+        }
+        return true;
+    } 
 }

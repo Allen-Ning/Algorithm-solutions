ab566bbd06f38051e5d5250892d94f09d833ab1c
Allen Ning <npc1222@icloud.com>
2023-02-27 00:12:59 +1100

    added improved java solution 35

--- java/Solution35.java
+++ java/Solution35.java
@@ -1,17 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int l = -1;
-        int h = nums.length;
+        int start = -1;
+        int end = nums.length;
 
-        while (l + 1 != h) {
-            int mid = l + (h - l) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[mid] > target) {
-                h = mid;
+        while (start + 1 != end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] < target) {
+                start = mid;
             } else {
-                l = mid;
+                end = mid;
             }
         }
-        return h;
+        return end;
     }
 }

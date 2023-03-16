cb458ac7e3a80550439b14d11f251ef7f9cbea9c
Allen Ning <npc1222@icloud.com>
2023-03-16 23:58:05 +1100

    added improved java solution 62

--- java/Solution62.java
+++ java/Solution62.java
@@ -1,20 +1,32 @@
 class Solution {
-  public int uniquePaths(int m, int n) {
-    int[][] dp = new int[m][n];
-    for (int i = 0; i < dp[0].length; i++) {
-      dp[0][i] = 1;
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
-    for (int i = 0; i < dp.length; i++) {
-      dp[i][0] = 1;
-    }
+    public int uniquePaths2(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[j] = 1;
+                    continue;
+                }
 
-    for (int i = 1; i < dp.length; i++) {
-      for ( int j = 1; j < dp[0].length; j++) {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-      }
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n -1];
     }
-
-    return dp[m - 1][n - 1];
-  }
 }

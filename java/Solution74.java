4eea6b5829496c019bce0d68a419a03d2682e929
Allen Ning <npc1222@icloud.com>
2023-04-04 21:35:40 +1000

    added improved java solution 74

--- java/Solution74.java
+++ java/Solution74.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = -1;
-        int j = matrix.length * matrix[0].length;
-        while (i + 1 != j) {
-            int mid = i + (j - i) / 2;
+        int start = -1;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int end = rows * cols;
+
+        while (start + 1 != end) {
+            int mid = start + (end - start) / 2;
 
             // trick -> map 1 dimension index to 2 dimensions indexes
-            int index = mid / matrix[0].length;
-            int subIndex = mid % matrix[0].length;
-
-            int value = matrix[index][subIndex];
-            if (value > target) {
-               j = mid;
-            } else if (value == target) {
-               return true;
+            if (matrix[mid / cols][mid % cols] <= target) {
+                start = mid;
             } else {
-               i = mid;
+                end = mid;
             }
         }
-        return false;
+        return start >= 0 && matrix[start / cols][start % cols] == target;
     }
 }

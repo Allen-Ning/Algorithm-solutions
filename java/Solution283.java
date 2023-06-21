3dc9b5a6f4e2b093824b2a2ff8cc4be49d806d4d
Allen Ning <npc1222@icloud.com>
2023-06-21 12:44:47 +1000

    added polished java solution 283

--- java/Solution283.java
+++ java/Solution283.java
@@ -1,16 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // left -> non-zero not including the current index
-        // right -> scan area
-        int left = 0;
-        int right = 0;
-        for (;right < nums.length; right++) {
-            int num = nums[right];
-            if (num == 0) continue;
+        // two pointer approach
+        // p1 indicates the current non-zero areas not including p1
+        // p2 is the scaning area, p2 will alawys stop at non-zero num
 
-            nums[left++] = num;
+        int p1 = 0;
+        for (int p2 = 0; p2 < nums.length; p2++) {
+            if (nums[p2] == 0) continue;
+
+            nums[p1++] = nums[p2];
         }
 
-        while (left < nums.length) nums[left++] = 0;
+        while (p1 < nums.length) nums[p1++] = 0;
     }
 }

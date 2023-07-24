b9e0dfa882c44881513e9d3d3a4f1efe00d286b0
Allen Ning <npc1222@icloud.com>
2023-07-24 23:12:28 +1000

    added polished java solution

--- java/Solution136.java
+++ java/Solution136.java
@@ -1,15 +1,10 @@
-class Solution136 {
-    public static void main(String[] args) {
-      Solution136 s = new Solution136();
-      int result = s.singleNumber(new int[]{1,1,2,2,3});
-      System.out.println(result);
-    }
-
+class Solution {
     public int singleNumber(int[] nums) {
-      int result = 0;
-      for (int num: nums) {
-        result ^= num;
-      }
-      return result;
+        // trick -> A ^ B ^ C ^ C ^ B = A
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
     }
 }

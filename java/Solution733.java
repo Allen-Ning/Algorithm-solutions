19812a5e9e937298cbe569a5f35f9f616a2b94bb
Allen Ning <npc1222@icloud.com>
2023-07-31 08:34:30 +1000

    added polished solution 733

--- java/Solution733.java
+++ java/Solution733.java
@@ -1,4 +1,42 @@
-class Solution {
+// bff
+class Solution1 {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int originColor = image[sr][sc];
+        // trick -> corner case - the starting pixel is same as color to filled
+        if (originColor == color) return image;
+
+        Queue<int[]> queue = new LinkedList();
+        queue.offer(new int[] {sr, sc});
+
+        int[][] dirs = new int[][] { {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (queue.size() > 0) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] point = queue.poll();
+                image[point[0]][point[1]] = color;
+
+                for (int[] dir : dirs) {
+                    int x = dir[0] + point[0];
+                    int y = dir[1] + point[1];
+
+                    if (x < 0 ||
+                        x >= image.length ||
+                        y < 0 ||
+                        y >= image[0].length ||
+                        image[x][y] != originColor
+                    ) continue;
+
+                    queue.offer(new int[] {x, y});
+                }
+            }
+        }
+        return image;
+    }
+}
+
+// dfs
+class Solution2 {
     // [1,1,2],
     // [1,1,0],
     // [1,0,1]
@@ -23,3 +61,5 @@ class Solution {
         helper(image, sr, sc - 1, oldColor, newColor);
     }
 }
+
+

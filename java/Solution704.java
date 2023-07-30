c22b221370da47bdc81a2f1346d956dca2dc710b
Allen Ning <npc1222@icloud.com>
2023-07-31 08:46:17 +1000

    added polished java solution

--- java/Solution704.java
+++ java/Solution704.java
@@ -1,18 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length;
+        int l = -1;
+        int h = nums.length;
 
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                end = mid;
+        while (l + 1 != h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] <= target) {
+                l = m;
             } else {
-                start = mid + 1;
+                h = m;
             }
         }
-        return -1;
+
+        // trick -> l could be -1, which is causing out of IndexOutOfBounds error
+        return (l >= 0 && nums[l] == target) ? l : -1;
     }
 }

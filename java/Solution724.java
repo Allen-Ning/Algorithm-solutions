12b55e051e0aa8c3928b1b3f92ceb066e4a1a709
Allen Ning <npc1222@icloud.com>
2023-09-10 22:20:35 +1000

    added a better java solution 724

--- java/Solution724.java
+++ java/Solution724.java
@@ -1,16 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        if (nums == null || nums.length == 0) return -1;
         int total = 0;
-        for (int num : nums) total += num;
-        int leftValue = 0;
-        int rightValue = total - nums[0];
-        if (leftValue == rightValue) return 0;
+        for (int num : nums) {
+            total += num;
+        }
 
-        for (int i = 1; i < nums.length; i++) {
-            leftValue += nums[i - 1];
-            rightValue -= nums[i];
-            if (leftValue == rightValue) return i;
+        int preSum = 0;
+        int right = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            right = total - preSum - num;
+            if (preSum == right) return i;
+            preSum += num;
         }
         return -1;
     }

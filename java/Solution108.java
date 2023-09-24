d046993dc188b91f586179d11a507cac2fccfdb3
Allen Ning <npc1222@icloud.com>
2023-09-25 09:21:35 +1000

    added polished solution

--- java/Solution108.java
+++ java/Solution108.java
@@ -8,20 +8,25 @@
  * }
  */
 class Solution {
-    private int[] nums;
-    
     public TreeNode sortedArrayToBST(int[] nums) {
-        this.nums = nums;
-        return helper(0, nums.length - 1);
+        /**
+            find middle
+
+            left -> left node
+            right -> right node
+            repeat the same step
+         */
+        return helper(0, nums.length - 1, nums);
     }
-    
-    private TreeNode helper(int start, int end) {
+
+    private TreeNode helper(int start, int end, int[] nums) {
         if (start > end) return null;
-        
+
         int mid = start + (end - start) / 2;
         TreeNode node = new TreeNode(nums[mid]);
-        node.left = helper(start, mid - 1);
-        node.right = helper(mid + 1, end);
+
+        node.left = helper(start, mid - 1, nums);
+        node.right = helper(mid + 1, end, nums);
         return node;
     }
 }

cc2567d9f2c2d18847e1dbeebe1abaea60bc3cd8
Allen Ning <npc1222@icloud.com>
2023-11-14 09:25:35 +1100

    added updated java solution 268

--- java/Solution268.java
+++ java/Solution268.java
@@ -1,21 +1,29 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int total = (0 + nums.length) * (nums.length + 1) / 2;
-        int subTotal = 0; 
+        int subTotal = 0;
         for (int i = 0; i < nums.length; i++) {
             subTotal += nums[i];
         }
         return total - subTotal;
     }
 
-    // a ^ a = 0
-    // b ^ 0 = b
-    // b ^ a ^ a = b
     public int missingNumber2(int[] nums) {
+        /**
+            trick ->
+            use ^ XOR operator
+            a ^ a = 0
+            a ^ 0 = a
+            b ^ a ^ a = b
+            (a ^ b) ^ c = a ^ (b ^ c)
+
+            check two cases:
+            [3,0,1] n = 3
+            [0,1,2] n = 3
+         */
         int result = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            result ^= i;
-            result ^= nums[i];
+            result ^= nums[i] ^ i;
         }
         return result;
     }

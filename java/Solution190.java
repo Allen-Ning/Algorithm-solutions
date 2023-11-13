a3ee5a9e83c353d485f3b3dff9c9bcde8dc2716e
Allen Ning <npc1222@icloud.com>
2023-11-13 20:56:58 +1100

    added polished java solution 190

--- java/Solution190.java
+++ java/Solution190.java
@@ -2,9 +2,14 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int result = 0;
-        for (int i = 0; i < 32; i++) {
-            int temp = (n >>> (31 - i)) & 1;
-            result += (temp << i); 
+        int count = 32;
+        while (count > 0) {
+            // trick -> there is no operation <<< in java
+            result = result << 1;
+            result += (n & 1);
+            n >>>= 1;
+
+            count--;
         }
         return result;
     }

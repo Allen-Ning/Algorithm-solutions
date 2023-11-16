6b16117a4c4c9d6f58888cf2b16cf6a92af4b306
Allen Ning <npc1222@icloud.com>
2023-11-17 08:58:33 +1100

    added polished java solution 191

--- java/Solution191.java
+++ java/Solution191.java
@@ -1,18 +1,16 @@
-public class Solution191 {
-  public static void main(String[] args) {
-    Solution191 s = new Solution191();
-    int val = s.hammingWeight(11);
-    System.out.println(val);
-  }
-
-  // you need to treat n as an unsigned value
-  public int hammingWeight(int n) {
-    int count = 0;
-    for (int i = 0; i < 32; i++) {
-      int value = n >> i;
-      int newValue = value & 1;
-      if (newValue == 1) count++;
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        /**
+            loop
+            >>> right operation and count 1
+         */
+        int result = 0;
+        while (n != 0) {
+            int bitValue = n & 1;
+            if (bitValue == 1) result++;
+            n >>>= 1;
+        }
+        return result;
     }
-    return count;
-  }
 }

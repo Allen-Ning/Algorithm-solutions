4964010d1640f12b320fe7d5a238436861d46108
Allen Ning <npc1222@icloud.com>
2023-11-24 09:51:37 +1100

    added polished java solution

--- java/Solution572.java
+++ java/Solution572.java
@@ -4,23 +4,40 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
-        if (isSame(s, t)) return true;
-        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        /**
+            1 -> find the node from root same as subRoot
+            2 -> compare
+            3 -> if same, return true
+            4 -> if not same, return false
+         */
+        // trick -> root: [1, 1, 1] and subRoot: [1] -> return true
+        if (subRoot == null) return false;
+        return findSameSubRoot(root, subRoot);
     }
 
-    private boolean isSame(TreeNode s, TreeNode t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
+    private boolean findSameSubRoot(TreeNode node, TreeNode subRoot) {
+        if (node == null) return false;
+        if (helper(node, subRoot)) return true;
 
-        if (s.val != t.val) return false;
+        return findSameSubRoot(node.left, subRoot) || findSameSubRoot(node.right, subRoot);
+    }
 
-        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    private boolean helper(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
+        if (node1.val != node2.val) return false;
+
+        return helper(node1.left, node2.left) && helper(node1.right, node2.right);
     }
 }

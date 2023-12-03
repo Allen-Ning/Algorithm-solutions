3cf29029c24b58662b645c8e692b380cafd561e4
Allen Ning <npc1222@icloud.com>
2023-12-04 09:03:44 +1100

    added polished java solution 994

--- java/Solution994.java
+++ java/Solution994.java
@@ -2,45 +2,42 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
 
-        Queue<int[]> queue = new LinkedList();
-        int total = 0;
         int counter = 0;
+        Queue<int[]> queue = new LinkedList();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1 || grid[i][j] == 2) total++;
-                if (grid[i][j] == 2) {
-                    queue.offer(new int[] {i, j});
-                    counter++;
-                }
+                int value = grid[i][j];
+                if (value == 2) queue.offer(new int[] {i, j});
+                if (value == 1) counter++;
             }
         }
 
-        if (total == counter) return 0;
+        if (counter == 0) return 0;
 
-        int[][] dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
         int step = 0;
-        while (!queue.isEmpty()) {
-            if (total == counter) return step;
+        while (queue.size() > 0) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                int[] current = queue.poll();
+                int[] point = queue.poll();
                 for (int[] dir : dirs) {
-                    int x = current[0] + dir[0];
-                    int y = current[1] + dir[1];
-                    if (x >= 0 &&
-                        y >= 0 &&
-                        x < grid.length &&
-                        y < grid[0].length &&
-                        grid[x][y] == 1
-                       ) {
-                        grid[x][y] = 2;
-                        queue.offer(new int[] {x, y});
-                        counter++;
-                    }
+                    int nextX = point[0] + dir[0];
+                    int nextY = point[1] + dir[1];
+                    if (nextX < 0 ||
+                        nextX >= grid.length ||
+                        nextY < 0 ||
+                        nextY >= grid[0].length ||
+                        grid[nextX][nextY] != 1
+                    ) continue;
+
+                    queue.offer(new int[] {nextX, nextY});
+                    counter--;
+                    grid[nextX][nextY] = 2;
                 }
             }
             step++;
         }
-        return -1;
+        // trick -> make to make a mistake about the final result returned is step - 1
+        return counter == 0 ? step - 1 : -1;
     }
 }

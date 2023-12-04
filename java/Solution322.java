03630f43bc0d7d864c2a8c952fa36b7a7794365a
Allen Ning <npc1222@icloud.com>
2023-12-05 07:07:47 +1100

    added polished java solution

--- java/Solution322.java
+++ java/Solution322.java
@@ -1,17 +1,15 @@
 class Solution {
-  public int coinChange(int[] coins, int amount) {
-    int[] dp = new int[amount + 1];
-    Arrays.fill(dp, -1);
-    dp[0] = 0;
-    for (int i = 1; i <= amount; i++) {
-      int min = Integer.MAX_VALUE;
-      for (int coin: coins) {
-        if (i >= coin && dp[i - coin] != -1) {
-          min = Math.min(min, dp[i - coin] + 1);
-          dp[i] = min;
-        } 
-      }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            if (dp[i] == 0) dp[i] = amount + 1;
+
+            for (int coin : coins) {
+                if (i - coin < 0) continue;
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
-    return dp[amount];
-  }
 }

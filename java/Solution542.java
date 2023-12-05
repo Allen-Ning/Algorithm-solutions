6224f95b77cc963a4b296ff3180ec95f0d674dcc
Allen Ning <npc1222@icloud.com>
2023-12-06 01:08:12 +1100

    added java solution 542

--- java/Solution542.java
+++ java/Solution542.java
@@ -1,43 +1,41 @@
 class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int[][] results = new int[matrix.length][matrix[0].length];
+    public int[][] updateMatrix(int[][] mat) {
+        int[][] results = new int[mat.length][mat[0].length];
         Queue<int[]> queue = new LinkedList();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] != 0) continue;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
                 // trick -> to use bfs reversed way
-                if (matrix[i][j] == 0) queue.offer(new int[] {i, j});
+                queue.add(new int[]{i, j});
             }
         }
 
-        helper(queue, matrix, results);
-        return results;
-    }
-
-    private void helper(Queue<int[]> queue, int[][] matrix, int[][] results) {
-        int[][] dirs = new int[][] {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
-        int step = 0;
+        int step = 1;
+        int[][] dirs = new int[][] {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
         while (queue.size() > 0) {
             int size = queue.size();
-            for (int index = 0; index < size; index++) {
-                int[] current = queue.poll();
+            for (int i = 0; i < size; i++) {
+                int[] point = queue.poll()
+
                 for (int[] dir : dirs) {
-                    int newX = current[0] + dir[0];
-                    int newY = current[1] + dir[1];
-                    if (newX >= 0 &&
-                        newX < matrix.length &&
-                        newY >= 0 &&
-                        newY < matrix[0].length &&
-                        matrix[newX][newY] == 1
-                        ) {
-                        matrix[newX][newY] = 0;
-                        queue.offer(new int[] {newX, newY});
-                        results[newX][newY] = step + 1;
-                    }
+                    int newX = point[0] + dir[0];
+                    int newY = point[1] + dir[1];
+
+                    if (newX < 0 ||
+                        newX >= mat.length ||
+                        newY < 0 ||
+                        newY >= mat[0].length ||
+                        mat[newX][newY] == 0
+                    ) continue;
+
+                    queue.offer(new int[]{newX, newY});
+                    mat[newX][newY] = 0;
+                    results[newX][newY] = step;
                 }
             }
             step++;
         }
+        return results;
     }
-
 }

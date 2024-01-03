7446dfa1c1fbf3c302514acd92752fab44ba90a6
Allen Ning <npc1222@icloud.com>
2024-01-04 00:00:28 +1100

    polished java solution 206

--- java/Solution206.java
+++ java/Solution206.java
@@ -3,20 +3,35 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-  public ListNode reverseList(ListNode head) {
-    ListNode current = head;
-    ListNode pre = null;
-    ListNode next = current.next;
-    while(current != null) {
-      current.next = pre;
-      pre = current;
-      current = next;
-      if (next != null) next = next.next;
+    public ListNode reverseList(ListNode head) {
+      /**
+      p  n
+         1 -> 2
+
+         p    n
+         1 -> 2
+
+              p    n
+         1 -> 2
+       */
+        return helper(head, null);
     }
-    return pre;
-  }
+
+    private ListNode helper(ListNode current, ListNode prev) {
+        // trick -> we need to return prev
+        if (current == null) return prev;
+
+        ListNode newHead = helper(current.next, current);
+
+        // trick -> the original head node will point to null
+        current.next = prev;
+
+        return newHead;
+    }
 }

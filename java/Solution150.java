51b340c3858d8a8c210d9b7f9dd0d653bf1b12c8
Allen Ning <npc1222@icloud.com>
2024-01-16 23:57:14 +1100

    added polished java solution 150

--- java/Solution150.java
+++ java/Solution150.java
@@ -1,36 +1,29 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        if (tokens == null || tokens.length == 0) return 0;
-        
-        Stack<Integer> s = new Stack();
-        for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i];
-            int addedValue = -1;
-            int value1 = -1;
-            int value2 = -1;
-            if (token.equals("+")) {
-                value1 = Integer.valueOf(s.pop());
-                value2 = Integer.valueOf(s.pop());
-                addedValue = value1 + value2;
-            } else if (token.equals("-")) {
-                value1 = Integer.valueOf(s.pop());
-                value2 = Integer.valueOf(s.pop());
-                addedValue = value2 - value1;
-            } else if (token.equals("*")) {
-                value1 = Integer.valueOf(s.pop());
-                value2 = Integer.valueOf(s.pop());
-                addedValue = value1 * value2;
-            } else if (token.equals("/")) {
-                value1 = Integer.valueOf(s.pop());
-                value2 = Integer.valueOf(s.pop());
-                addedValue = value2 / value1;
-            } else {
-                addedValue = Integer.valueOf(token);
-            }
-            s.push(addedValue);
-            
-        }
-        return s.pop();
-    }
+        if (tokens == null) return 0;
 
+        Stack<Integer> stack = new Stack();
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                Integer value = stack.pop();
+                Integer value2 = stack.pop();
+                stack.push(value + value2);
+            } else if (token.equals("-")) {
+                Integer value = stack.pop();
+                Integer value2 = stack.pop();
+                stack.push(value2 - value);
+            } else if (token.equals("*")) {
+                Integer value = stack.pop();
+                Integer value2 = stack.pop();
+                stack.push(value2 * value);
+            } else if (token.equals("/")) {
+                Integer value = stack.pop();
+                Integer value2 = stack.pop();
+                stack.push(value2 / value);
+            } else {
+                stack.push(Integer.valueOf(token));
+            }
+        }
+        return stack.pop();
+    }
 }

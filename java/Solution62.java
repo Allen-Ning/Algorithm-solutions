b6be9aefb3445cac561bf50294277280d8e68ff1
Allen Ning <npc1222@icloud.com>
2024-01-23 23:09:49 +1100

    added polished java solution 62

--- java/Solution62.java
+++ java/Solution62.java
@@ -17,16 +17,13 @@ class Solution {
 
     public int uniquePaths2(int m, int n) {
         int[] dp = new int[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    dp[j] = 1;
-                    continue;
-                }
+        Arrays.fill(dp, 1);
 
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 dp[j] += dp[j - 1];
             }
         }
-        return dp[n -1];
+        return dp[n - 1];
     }
 }

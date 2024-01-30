18dc88da3f74687a9fba87472bd79170f5cbf34a
Allen Ning <npc1222@icloud.com>
2024-01-31 08:08:29 +1100

    added polished java solution 54

--- java/Solution54.java
+++ java/Solution54.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> results = new ArrayList();
-
-        helper(results, matrix, 0, 0, 0);
-        return results;
+        List<Integer> result = new ArrayList();
+        int total = matrix.length * matrix[0].length;
+        // right, down, left, up
+        int[][] dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        helper(result, matrix, dirs, total, 0, 0, 0);
+        return result;
     }
 
-    private void helper(List<Integer> results, int[][] matrix, int x, int y, int direction) {
-        if (x < 0 ||
-            x >= matrix.length ||
-            y < 0 ||
-            y >= matrix[0].length ||
-            matrix[x][y] < -100
-           ) return;
+    // trick -> special case
+    //          e.g.
+    //                 [1, 2, 3, 4]
+    //                 [5, 6, 7, 8]
+    //                 [9, 10,11,12]
+    //                 [13,14,15,16]
+    //
+    //          result [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
+    private void helper(List<Integer> result, int[][] matrix, int[][] dirs, int total, int i , int j, int baseIndex) {
+        if (i < 0 || i >= matrix.length) return;
+        if (j < 0 || j >= matrix[0].length) return;
+        if (matrix[i][j] > 100) return;
 
-        results.add(matrix[x][y]);
-        matrix[x][y] = -200;
+        result.add(matrix[i][j]);
+        matrix[i][j] = 101;
 
-        int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        // trick -> keep the current direction if possible
-        helper(results, matrix, x + directions[direction][0], y + directions[direction][1], direction);
-
-        // trick -> if not cannot keep the current direction,
-        //          will try other direction based on the priority (right, down, left, up)
-        for (int i = 0; i < directions.length; i++) {
-            if (i == direction) continue;
-
-            int nextX = x + directions[i][0];
-            int nextY = y + directions[i][1];
-            helper(results, matrix, nextX, nextY, i);
+        // trick -> keep track the current direction if possible
+        for (int z = 0; z < dirs.length; z++) {
+            int index = (baseIndex + z) % dirs.length;
+            helper(result, matrix, dirs, total, i + dirs[index][0], j + dirs[index][1], index);
         }
     }
 }

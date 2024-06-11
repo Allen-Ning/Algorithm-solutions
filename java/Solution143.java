db3e5e0ab5803973368650b1e0ba822518fb1a31
Allen Ning <npc1222@icloud.com>
2024-06-11 20:42:16 +1000

    added polished java solution 143

--- java/Solution143.java
+++ java/Solution143.java
@@ -3,55 +3,53 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-  public void reorderList(ListNode head) {
-    if (head == null) return;
-    ListNode mid = findMiddleNode(head);
+    public void reorderList(ListNode head) {
+        // trick -> only one node
+        if (head == null || head.next == null) return;
 
-    // reverse the mid
-    // 1 -> 2 -> 3 <- 4
-    // 1 -> 2 -> 3 <- 4 <- 5
-    ListNode end = reverse(mid);
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
 
-    ListNode node = head;
-    while (node != mid && end != mid) {
-      ListNode next = node.next;
-      ListNode endNext = end.next;
-      node.next = end;
-      end.next = next;
-      node = next;
-      end = endNext;
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        ListNode secondHalfHead = reverse(mid, null);
+
+        ListNode current = dummy;
+        while (head != null && secondHalfHead != null) {
+            current.next = head;
+            head = head.next;
+
+            current = current.next;
+
+            current.next = secondHalfHead;
+            secondHalfHead = secondHalfHead.next;
+
+            current = current.next;
+        }
+
+        if (head != null) current.next = head;
+        if (secondHalfHead != null) current.next = secondHalfHead;
     }
 
-  }
+    private ListNode reverse(ListNode node, ListNode prev) {
+        if (node == null) return prev;
 
-  private ListNode findMiddleNode(ListNode node) {
-    ListNode slow = node;
-    ListNode fast = node;
-    while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
+        ListNode tail = reverse(node.next, node);
+        node.next = prev;
+        return tail;
     }
-    return slow;
-  }
-
-  private ListNode reverse(ListNode node) {
-    ListNode pre = null;
-    ListNode current = node;
-    ListNode next = current.next;
-    while(current != null) {
-      next = current.next;
-      current.next = pre;
-
-      pre = current;
-      current = next;
-
-    }
-    return pre;
-
-  }
 }

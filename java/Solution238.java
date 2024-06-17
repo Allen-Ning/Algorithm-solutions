fc0bef221654302cbf3bfa25139e7c9551ba00ed
Allen Ning <npc1222@icloud.com>
2024-06-18 08:32:51 +1000

    added java solution 238

--- java/Solution238.java
+++ java/Solution238.java
@@ -1,17 +1,20 @@
 class Solution {
-  public int[] productExceptSelf(int[] nums) {
-    int[] sum = new int[nums.length];
-    sum[0] = 1;
+    public int[] productExceptSelf(int[] nums) {
+        int[] results = new int[nums.length];
+        results[0] = 1;
+        // trick -> using int[] results to record
+        //          from left to right multiply not including the current value
+        for (int i = 1; i < nums.length; i++) {
+            results[i] = results[i - 1] * nums[i - 1];
+        }
 
-    for (int i = 1; i < nums.length; i++) {
-      sum[i] = sum[i - 1] * nums[i - 1];
+        int right = 1;
+        // trick -> using variable right to record
+        //          from right to left multiply not including the current value
+        for (int i = nums.length - 1; i >= 0; i--) {
+            results[i] *= right;
+            right *= nums[i];
+        }
+        return results;
     }
-    
-    int right = 1;
-    for (int i = nums.length - 1; i >= 0; i--) {
-      sum[i] = sum[i] * right;
-      right = right * nums[i];
-    }
-    return sum;
-  }
 }

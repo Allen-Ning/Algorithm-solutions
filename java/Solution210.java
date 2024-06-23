049d81efdc838eae5646004a17ab8d15e6c91547
Allen Ning <npc1222@icloud.com>
2024-06-23 16:57:57 +1000

    simplified java solution 210

--- java/Solution210.java
+++ java/Solution210.java
@@ -1,56 +1,51 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // trick -> corner case1
-        // numCourses is zero -> return empty
-        if (numCourses == 0) return new int[0];
-
-        // preprocesing
-        HashMap<Integer, List<Integer>> courses = new HashMap();
         int[] indegrees = new int[numCourses];
+        Set[] dependentCourses = new Set[numCourses];
+        Queue<Integer> queue = new LinkedList();
+        List<Integer> results = new ArrayList();
 
         for (int[] prerequisite : prerequisites) {
-            int first = prerequisite[1];
-            int second = prerequisite[0];
-            indegrees[second]++;
+            int course = prerequisite[1];
+            int nextCourse = prerequisite[0];
+            indegrees[nextCourse] += 1;
 
-            List<Integer> nextCourses = courses.getOrDefault(first, new ArrayList());
-            nextCourses.add(second);
-            courses.put(first, nextCourses);
+            Set<Integer> set = dependentCourses[course];
+            if (set == null) {
+                set = new HashSet();
+                dependentCourses[course] = set;
+            }
+            set.add(nextCourse);
         }
 
-        List<Integer> results = new ArrayList();
-        Queue<Integer> queue = new LinkedList();
-        boolean[] isVisited = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] > 0) continue;
 
-        // trick -> add all course whose indegree is zero
-        for (int i = 0; i < indegrees.length; i++) {
-            if (indegrees[i] != 0) continue;
-            queue.offer(i);
+            queue.offer(i); 
         }
 
         while (queue.size() > 0) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                // trick -> syntax
                 Integer course = queue.poll();
                 results.add(course);
-                isVisited[course] = true;
-                List<Integer> nextCourses = courses.get(course);
 
+                Set<Integer> nextCourses = dependentCourses[course];
                 if (nextCourses == null) continue;
-                for (int nextCourse : nextCourses) {
-                    if (isVisited[nextCourse]) continue;
-                    indegrees[nextCourse]--;
 
-                    if (indegrees[nextCourse] == 0) queue.offer(nextCourse);
+                for (Integer nextCourse : nextCourses) {
+                    if (--indegrees[nextCourse] > 0) continue;
+
+                    queue.offer(nextCourse);
                 }
             }
         }
 
-        // trick -> corner case2
+        // trick -> corner case
         // impossible to finish all courses -> return empty
-        if (results.size() < numCourses) return new int[0];
-        int[] finalResults = new int[results.size()];
+        if (results.size() != numCourses) return new int[] {};
+
+        int[] finalResults = new int[numCourses];
         for (int i = 0; i < results.size(); i++) {
             finalResults[i] = results.get(i);
         }

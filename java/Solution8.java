450b217fcd11711501fda3846e9136e432d49714
Allen Ning <npc1222@icloud.com>
2024-08-01 22:18:59 +1000

    added polished java solution 8

--- java/Solution8.java
+++ java/Solution8.java
@@ -1,39 +1,46 @@
 class Solution {
-    public int myAtoi(String str) {
-        str = str.trim();
-        if (str.length() == 0) return 0;
-
-        int index = 0;
+    public int myAtoi(String s) {
+        int i = 0;
         int sign = 1;
-        int num = 0;
-        boolean started = false;
-
-        // check the sign seperate
-        char firtLetter = str.charAt(0);
-        if (firtLetter == '-') {
-            sign = -1;
-            index++;
-        } else if (firtLetter == '+') {
-            index++;
-        }
-
-        while (index < str.length()) {
-            char c = str.charAt(index);
-            if (c - '0' >= 0 && '9' - c >= 0) {
-                // notice: implementation trick
-                // cannot do value = num * 10 + c - '0';
-                // num * 10 may lead to overflow before assigning to long value
-                long value = num;
-                value = value * 10 + c - '0';
-                if (value * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-                if (value * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-                num = num * 10 + c - '0';
-                started = true;
-            } else {
-                return started ? sign * num : 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') break;
+            if (c == '+') {
+                i++;
+                break;
             }
-            index++;
+
+            if (c == '-') {
+                sign = -1;
+                i++;
+                break;
+            }
+
+            if ((c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '.'
+            ) {
+                return 0;
+            }
+
+            i++;
         }
-        return sign * num;
+
+        long result = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (c > '9' || c < '0') break;
+
+            // notice: implementation trick
+            // cannot do value = num * 10 + c - '0';
+            // num * 10 may lead to overflow before assigning to long value
+            result = result * 10 + (long)(c - '0');
+            if (result * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (result  * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+        }
+
+        result = result * sign;
+        return (int)result;
     }
 }

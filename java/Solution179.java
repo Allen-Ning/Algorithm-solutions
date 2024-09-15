e8cdf554a1e3889d76b5288a73ba4aeb40d2cd85
Allen Ning <npc1222@icloud.com>
2024-09-15 16:35:42 +1000

    add polished java solution 179

--- java/Solution179.java
+++ java/Solution179.java
@@ -1,44 +1,37 @@
 class Solution {
-  public String largestNumber(int[] nums) {
-    if (nums == null || nums.length == 0) return "";
+    public String largestNumber(int[] nums) {
+        String[] numbers = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) numbers[i] = String.valueOf(nums[i]);
 
-    String[] numStringArray = new String[nums.length];
-    for (int i = 0; i < numStringArray.length; i++) {
-      numStringArray[i] = String.valueOf(nums[i]);
-    }
-    
-    String[] sortedNums = sort(numStringArray);
-    if (sortedNums[0].equals("0")) return "0";
-    StringBuilder str = new StringBuilder();
-    for (String num: sortedNums) {
-      str.append(num);
-    }
-    return str.toString();
-  }
-  
-  // [30, 3]
-  // num1 3
-  // num2 30
-  // concat 1 = 303
-  // concat 2 =  330
-  // -1
-  // [3, 30]
+        Arrays.sort(numbers, (a, b) -> compare(a, b));
 
-  // [3, 30]
-  // num1 30
-  // num2 3
-  // concat 1 = 330
-  // concat 2 = 303
-  // 1
-  private String[] sort(String[] nums) {
-    Arrays.sort(nums, new Comparator<String>() {
-      @Override
-      public int compare(String num1, String num2) {
-        String concat1 = num2 + num1;
-        String concat2 = num1 + num2;
-        return concat1.compareTo(concat2);
-      }
-    });
-    return nums;
-  }
+        StringBuilder sb = new StringBuilder();
+        if (numbers[0].equals("0")) return numbers[0];
+        for (String num : numbers) sb.append(num);
+        return sb.toString();
+    }
+    // [30, 3]
+    // num1 3
+    // num2 30
+    // concat 1 = 303
+    // concat 2 =  330
+    // -1
+    // [3, 30]
+
+    // [3, 30]
+    // num1 30
+    // num2 3
+    // concat 1 = 330
+    // concat 2 = 303
+    // 1
+    private int compare(String a, String b) {
+        // e.g a -> 10, b -> 2
+        // 102
+        String result1 = a + b;
+
+        // 210
+        String result2 = b + a;
+
+        return result2.compareTo(result1);
+    }
 }

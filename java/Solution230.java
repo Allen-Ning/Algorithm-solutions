31812b68b62c41e9a3e6431d59fa08bd7a5dbc4d
Allen Ning <npc1222@icloud.com>
2024-09-15 15:32:22 +1000

    polish java solution 230

--- java/Solution230.java
+++ java/Solution230.java
@@ -4,29 +4,33 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        Stack<TreeNode> stack = new Stack();
-        int count = 0;
-        TreeNode node = root;
-        while(node != null) {
-          stack.push(node);
-          node = node.left;
-        }
+        /**
+            dfs
 
-        while (!stack.isEmpty()) {
-          node = stack.pop();
-          count++;
-          if (count == k) return node.val;
-          node = node.right;
-          while(node != null) {
-            stack.push(node);
-            node = node.left;
-          }
-        }
-        return -1;
-  }
+            in-order traversal
+        */
+        List<Integer> result = new ArrayList();
+        helper(result, root, k);
+        return result.get(k - 1);
+    }
+
+    private void helper(List<Integer> result, TreeNode node, int k) {
+        if (node == null) return;
+        if (result.size() >= k) return;
+
+        helper(result, node.left, k);
+        result.add(node.val);
+        helper(result, node.right, k);
+    }
 }

a9fe9ddab5c4d8b64c3702b25389b426b392a2e0
Allen Ning <npc1222@icloud.com>
2024-10-16 07:23:49 +1100

    added comment for corner case

--- java/Solution24.java
+++ java/Solution24.java
@@ -15,6 +15,7 @@ class Solution {
 
     private ListNode helper(ListNode node) {
         if (node == null) return null;
+        // trick -> this is a corner case when there is only one node left
         if (node.next == null) return node;
 
         ListNode next = node.next;

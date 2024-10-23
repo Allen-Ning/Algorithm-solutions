5a35411a7a18f8b82837f78b296594cee522a76a
Allen Ning <npc1222@icloud.com>
2024-10-23 21:47:36 +1100

    added another solution for 361

--- java/Solution362.java
+++ java/Solution362.java
@@ -26,6 +26,38 @@ class HitCounter {
     }
 }
 
+class HitCounter2 {
+    Deque<int[]> deque;
+
+    public HitCounter() {
+        this.deque = new ArrayDeque<int[]>();
+    }
+
+    public void hit(int timestamp) {
+        int[] value = deque.peekLast();
+        if (value != null && value[0] == timestamp) {
+            value[1] += 1;
+        } else {
+            value = new int[] {timestamp, 1};
+            deque.addLast(value);
+        }
+    }
+
+    public int getHits(int timestamp) {
+        while (deque.size() > 0) {
+            int[] value = deque.peekFirst();
+            if (timestamp - value[0] < 300) break;
+
+            deque.pollFirst();
+        }
+
+        int result = 0;
+        for (int[] value : deque) {
+            result += value[1];
+        }
+        return result;
+    }
+}
 /**
  * Your HitCounter object will be instantiated and called as such:
  * HitCounter obj = new HitCounter();

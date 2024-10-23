cbec8a898afe5088c332520494c14a8332abccfd
Allen Ning <npc1222@icloud.com>
2024-10-23 22:03:05 +1100

    fixed a bug for java solution 198

--- java/Solution198.java
+++ java/Solution198.java
@@ -1,13 +1,16 @@
 class Solution {
-  public int rob(int[] nums) {
-    int[] r = new int[nums.length];
-    int[] n = new int[nums.length];
-    r[0] = nums[0];
-    n[0] = 0;
-    for (int i = 1; i < nums.length; i++) {
-      r[i] = n[i -1] + nums[i];
-      n[i] = r[i -1];
+    public int rob(int[] nums) {
+        int[] rob = new int[nums.length];
+        int[] notRob = new int[nums.length];
+        rob[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            rob[i] = notRob[i - 1] + nums[i];
+            // trick ->  [2, 1, 1,           2]
+            //         r  2  1  3            4
+            //         n  0  2  2 max(1, 2)  3
+            notRob[i] = Math.max(rob[i - 1], notRob[i - 1]);
+        }
+        return Math.max(rob[nums.length - 1], notRob[nums.length - 1]);
     }
-    return Math.max(r[nums.length -1], n[nums.length -1]);
-  }
 }

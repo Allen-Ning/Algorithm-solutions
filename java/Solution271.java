bbb34f9809c85ff514a79a136207f5c6dbe68f74
Allen Ning <npc1222@icloud.com>
2024-11-06 08:07:23 +1100

    added a polished java solution 271

--- java/Solution271.java
+++ java/Solution271.java
@@ -1,37 +1,38 @@
 public class Codec {
-
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
         for (String str : strs) {
-            sb.append(str.length()).append("/").append(str);
+            int len = str.length();
+            sb.append(len + "_" + str);
         }
         return sb.toString();
     }
 
+    /**
+        e.g. 5_Hello5_World
+     */
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> result = new ArrayList();
-
-        int index = 0;
-        int size = 0;
         StringBuilder sb = new StringBuilder();
-        while (index < s.length()) {
-            char c = s.charAt(index);
-            if (c == '/') {
-                if (size == 0) sb.append("");
-                while (size > 0) {
-                    sb.append(s.charAt(index + 1));
-                    index++;
-                    size--;
-                }
-                result.add(sb.toString());
-                // trick -> this setLenght method is to clear StringBuilder object
-                sb.setLength(0);
-            } else {
-                size = size * 10 + (c - '0');
+        int start = 0;
+        int length = s.length();
+        while (start < length) {
+            int num = 0;
+            while (s.charAt(start) != '_') {
+                int val = s.charAt(start) - '0';
+                num = num * 10 + val;
+                start++;
+            } 
+
+            start++;
+            sb = new StringBuilder();
+            while (num > 0) {
+                sb.append(s.charAt(start++));
+                num--;
             }
-            index++;
+            result.add(sb.toString());
         }
         return result;
     }

1acf1811f256e9bf335438b94e98600d4108b4f3
Allen Ning <npc1222@icloud.com>
2025-05-09 08:33:37 +1000

    polished java solution 91

--- java/Solution91.java
+++ java/Solution91.java
@@ -5,14 +5,14 @@ class Solution {
     dp[1] = s.charAt(0) != '0' ? 1 : 0;
 
     for (int i = 2; i < dp.length; i++) {
-      String oneLetter = s.substring(i - 1, i);
-      String twoLetters = s.substring(i - 2, i);
+      Integer oneLetter = Integer.valueOf(s.substring(i - 1, i));
+      Integer twoLetters = Integer.valueOf(s.substring(i - 2, i));
 
-      if (1 <= Integer.valueOf(oneLetter) && Integer.valueOf(oneLetter) <= 9) {
+      if (1 <= oneLetter && oneLetter <= 9) {
         dp[i] += dp[i -1];
       }
 
-      if (10 <= Integer.valueOf(twoLetters) && Integer.valueOf(twoLetters) <= 26) {
+      if (10 <= twoLetters && twoLetters <= 26) {
         dp[i] += dp[i -2];
       }
     }

63632f1f5f43ce372c831fef0fb0a3cdb16f11e5
Allen Ning <npc1222@icloud.com>
2026-03-12 13:46:21 +1100

    added polsihed solution 328

--- java/Solution328.java
+++ java/Solution328.java
@@ -1,41 +1,37 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *   int val;
+ *   ListNode next;
+ *   ListNode() {}
+ *   ListNode(int val) { this.val = val; }
+ *   ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        /**
-               o.   e.   o.   e.   o.
-            // 1 -> 2 -> 3 -> 4 -> 5 -> null
+        if (head == null) return head;
 
-               o.   e.   o.   e.   o.   e.
-            // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null -> null
-         */
-        ListNode oddHead = head;
-        ListNode evenHead = head.next;
-        ListNode oddCurrent = oddHead;
-        ListNode evenCurrent = evenHead;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        // trick -> change odd.next first because odd is before even
+        // o. e
+        // e.g. 1 -> 2 -> 3 -> 4 -> 5 -> null
+        // 1 -> 2 -> 4 -> 5 -> null (if we move even first)
+        while (even != null && even.next != null) {
+            ListNode oddNext = even.next;
+            ListNode evenNext = even.next.next;
 
-        while (oddCurrent != null && oddCurrent.next != null && oddCurrent.next.next != null) {
-            // trick -> change odd.next first because odd is before even
-            //                o.   e
-            //          e.g.  1 -> 2 -> 3 -> 4 -> 5 -> null
-            //                1 -> 2 -> 4 -> 5 -> null (if we move even first)
-            oddCurrent.next = oddCurrent.next.next;
-            evenCurrent.next = evenCurrent.next.next;
+            odd.next = oddNext;
+            odd = oddNext;
 
-            oddCurrent = oddCurrent.next;
-            evenCurrent = evenCurrent.next;
+            even.next = evenNext;
+            even = even.next;
         }
 
-        oddCurrent.next = evenHead;
-        return oddHead;
+        // the odd is the tail
+        odd.next = evenHead;
+        return head;
     }
 }

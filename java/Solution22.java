24c4694284ccf9c525747e8da643b52946076ac5
Allen Ning <npc1222@icloud.com>
2026-03-19 23:01:41 +1100

    added polished java solution22

--- java/Solution22.java
+++ java/Solution22.java
@@ -13,6 +13,31 @@ class Solution {
         }
 
         helper(results, n, left + 1, right, str + "(");
-        helper(results, n, left, right + 1, str + ")");       
+        helper(results, n, left, right + 1, str + ")");
+    }
+}
+
+class Solution2 {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList();
+        helper(n, 0, 0, new StringBuilder(), result);
+        return result;
+    }
+
+    private void helper(int n, int l, int r, StringBuilder sb, List<String> result) {
+        if (r > l) return;
+        if (l > n) return;
+        if (r > n) return;
+
+        if (l == n && r == n) {
+            result.add(sb.toString());
+            return;
+        }
+
+        helper(n, l + 1, r, sb.append("("), result);
+        // trick -> syntax
+        sb.deleteCharAt(sb.length() - 1);
+        helper(n, l, r + 1, sb.append(")"), result);
+        sb.deleteCharAt(sb.length() - 1);
     }
 }

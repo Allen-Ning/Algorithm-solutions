1864ae0949f57ae99491a692f089094f54dd64bf
Allen Ning <npc1222@icloud.com>
2026-08-10 14:49:14 +1000

    polished java solution 703

--- java/Solution703.java
+++ java/Solution703.java
@@ -1,23 +1,23 @@
 class KthLargest {
-
     PriorityQueue<Integer> minHeap;
     int k;
+
     public KthLargest(int k, int[] nums) {
-        minHeap = new PriorityQueue<Integer>();
-        for (int num : nums) {
-            minHeap.offer(num);
+        // trick -> small value stays in the head and pull first
+        this.minHeap = new PriorityQueue<Integer>((a, b) -> a - b);
+        this.k = k;
+
+        for (int i = 0; i < nums.length; i++) {
+            minHeap.offer(nums[i]);
+
             if (minHeap.size() > k) minHeap.poll();
         }
-        this.k = k;
     }
 
     public int add(int val) {
-        if (minHeap.size() < k) {
-            minHeap.offer(val);
-        } else if (val > minHeap.peek()) {
-           minHeap.offer(val);
-           minHeap.poll();
-        }
+        minHeap.offer(val);
+        if (minHeap.size() > k) minHeap.poll();
+
         return minHeap.peek();
     }
 }

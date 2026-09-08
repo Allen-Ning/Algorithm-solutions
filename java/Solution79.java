5e0dafc00c293fb33fe30a380482b98f37886dd4
Allen Ning <npc1222@icloud.com>
2026-09-08 23:47:27 +1000

    added polished java solution 79

--- java/Solution79.java
+++ java/Solution79.java
@@ -1,32 +1,34 @@
 class Solution {
-  public boolean exist(char[][] board, String word) {
-      for (int i = 0; i < board.length; i++) {
-          for (int j = 0; j < board[0].length; j++) {
-               if (helper(board, word, i, j, 0)) {
-                   return true;
-               }
-          }
-      }
-      return false;
-  }
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (!helper(board, i, j, 0, word)) continue;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-  private boolean helper(char[][] board, String word, int x, int y, int index) {
-      if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
-      if (index >= word.length()) return false;
-      if (board[x][y] != word.charAt(index)) return false;
-      if (index == word.length() - 1 && board[x][y] == word.charAt(index)) return true;
+    private boolean helper(char[][] board, int i, int j, int index, String word) {
+        if (index >= word.length()) return true;
+        if (i < 0 ||
+            j < 0 ||
+            i >= board.length ||
+            j >= board[0].length ||
+            board[i][j] == '0' ||
+            board[i][j] != word.charAt(index)
+        ) return false;
 
-      board[x][y] = '*';
-      int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-      for (int[] dir : dirs) {
-          int nextX = dir[0] + x;
-          int nextY = dir[1] + y;
+        board[i][j] = '0';
+        boolean result = helper(board, i + 1, j, index + 1, word) ||
+            helper(board, i - 1, j, index + 1, word) ||
+            helper(board, i, j + 1, index + 1, word) ||
+            helper(board, i, j - 1, index + 1, word);
 
-          if (helper(board, word, nextX, nextY, index + 1)) return true;
-      }
-
-      // trick -> easy to forget reset the value
-      board[x][y] = word.charAt(index);
-      return false;
-  }
+        // trick -> easy to forget reset the value
+        board[i][j] = word.charAt(index);
+        return result;
+    }
 }
